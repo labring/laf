20c3802597cfab02b71450ed504d6bdbd73b98d8
maslow <wangfugen@126.com>
2021-03-22 18:49:57 +0800

    #doc 更新文档；

--- src/config.ts
+++ src/config.ts
@@ -4,12 +4,13 @@ export default class Config {
   static get db() {
     return {
       database: process.env['DB'] ?? 'less_framework',
-      user: process.env['DB_USER'] ?? "root",
-      password: process.env['DB_PASSWORD'] ?? "kissme",
-      host: process.env['DB_HOST'] ?? "127.0.0.1",
-      port: (process.env['PORT'] ?? 3306) as number,
       uri: process.env['DB_URI'] ?? 'mongodb://localhost:27017',
-      poolSize: (process.env['DB_POOL_LIMIT'] ?? 100) as number
+      poolSize: (process.env['DB_POOL_LIMIT'] ?? 100) as number,
+
+      // user: process.env['DB_USER'] ?? "root",
+      // password: process.env['DB_PASSWORD'] ?? "kissme",
+      // host: process.env['DB_HOST'] ?? "127.0.0.1",
+      // port: (process.env['PORT'] ?? 3306) as number,
     }
   }
 
