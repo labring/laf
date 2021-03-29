0107f03ec1cc491c138b343d688a295c63c91933
maslow <wangfugen@126.com>
2021-03-29 21:06:12 +0800

    fix(init): add rules & permission to init.js

--- init/index.js
+++ init/index.js
@@ -6,39 +6,8 @@ const assert = require('assert')
 const { MongoAccessor, getDb } = require('less-api')
 const adminRules = require('./rules/admin.json')
 const appRules = require('./rules/app.json')
+const { permissions } = require('./permissions')
 
-const permissions = [
-  { name: 'role.create', label: '创建角色' },
-  { name: 'role.read', label: '读取角色' },
-  { name: 'role.edit', label: '编辑角色' },
-  { name: 'role.delete', label: '删除角色' },
-
-  { name: 'permission.create', label: '创建权限' },
-  { name: 'permission.read', label: '读取权限' },
-  { name: 'permission.edit', label: '编辑权限' },
-  { name: 'permission.delete', label: '删除权限' },
-
-  { name: 'admin.create', label: '创建管理员' },
-  { name: 'admin.read', label: '获取管理员' },
-  { name: 'admin.edit', label: '编辑管理员' },
-  { name: 'admin.delete', label: '删除管理员' },
-
-  { name: 'rule.create', label: '创建访问规则' },
-  { name: 'rule.read', label: '获取访问规则' },
-  { name: 'rule.edit', label: '编辑访问规则' },
-  { name: 'rule.delete', label: '删除访问规则' },
-  { name: 'rule.apply', label: '应用访问规则' },
-
-  { name: 'function.create', label: '创建云函数' },
-  { name: 'function.read', label: '获取云函数' },
-  { name: 'function.edit', label: '编辑云函数' },
-  { name: 'function.delete', label: '删除云函数' },
-  { name: 'function.publish', label: '发布云函数' },
-  { name: 'function.debug', label: '调试云函数' },
-
-  { name: 'function_logs.read', label: '查看云函数日志' },
-  { name: 'function_logs.delete', label: '查看云函数日志' }
-]
 
 const accessor = new MongoAccessor(Config.db.database, Config.db.uri, {
   useNewUrlParser: true,
