fe4b9068527c5b62b497033289b2f0190b548e1d
maslow <wangfugen@126.com>
2021-06-21 15:59:11 +0800

    chore: 移除 lsf 中用户登陆注册接口（已使用内置云函数代替）

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,6 @@ import { AdminEntryRouter } from './entry/admin'
 import { AppEntryRouter } from './entry/app'
 import { FileRouter } from './file/index'
 import { FunctionRouter } from './function/index'
-import { UserRouter } from './user/index'
 
 export const router = Router()
 
@@ -12,7 +11,6 @@ router.use('/admin', AdminEntryRouter)
 router.use('/admin', AdminRouter)
 
 router.use('/app', AppEntryRouter)
-router.use('/app', UserRouter)
 
 router.use('/file', FileRouter)
 router.use('/func', FunctionRouter)
--- src/router/user/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import { LoginRouter } from './login'
-import { RegisterRouter } from './register'
-
-export const UserRouter = Router()
-
-UserRouter.use('/user', LoginRouter)
-UserRouter.use('/user', RegisterRouter)
