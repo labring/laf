cdafdb13aa38bbace1e315e10eb7a5938296acb7
maslow <wangfugen@126.com>
2021-08-02 17:13:02 +0800

    feat: 增加 create 函数；

--- packages/app-server/src/cloud-sdk/index.ts
+++ packages/app-server/src/cloud-sdk/index.ts
@@ -14,8 +14,8 @@ import { invokeInFunction } from "./invoke"
 
 export type InvokeFunctionType = (name: string, param: FunctionContext) => Promise<FunctionResult>
 export type EmitFunctionType = (event: string, param: any) => void
-export type GetTokenFunctionType = (payload: any) => string
-export type ParseTokenFunctionType = (token: string) => any | null
+export type GetTokenFunctionType = (payload: any, secret?: string) => string
+export type ParseTokenFunctionType = (token: string, secret?: string) => any | null
 
 export interface CloudSdkInterface {
   fetch: AxiosStatic
@@ -30,6 +30,9 @@ export interface CloudSdkInterface {
 }
 
 
+/**
+ * Cloud SDK 实例
+ */
 const cloud: CloudSdkInterface = {
   database: () => Globals.createDb(),
   storage: (namespace: string) => new LocalFileStorage(Config.LOCAL_STORAGE_ROOT_PATH, namespace),
@@ -42,5 +45,30 @@ const cloud: CloudSdkInterface = {
   mongodb: Globals.accessor.db
 }
 
+/**
+ * 等数据库连接成功后，更新其 mongo 对象，否则为 null
+ */
+Globals.accessor.ready.then(() => {
+  cloud.mongodb = Globals.accessor.db
+})
+
+/**
+ * 创建一个 SDK 实例
+ * @returns 
+ */
+export function create() {
+  const cloud: CloudSdkInterface = {
+    database: () => Globals.createDb(),
+    storage: (namespace: string) => new LocalFileStorage(Config.LOCAL_STORAGE_ROOT_PATH, namespace),
+    fetch: request,
+    invoke: invokeInFunction,
+    emit: (event: string, param: any) => SchedulerInstance.emit(event, param),
+    shared: CloudFunction._shared_preference,
+    getToken: getToken,
+    parseToken: parseToken,
+    mongodb: Globals.accessor.db
+  }
+  return cloud
+}
 
 export default cloud
