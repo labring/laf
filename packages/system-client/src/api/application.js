0aa92883706d56981c4fe365e0134b4be598586c
maslow <wangfugen@126.com>
2021-09-02 17:51:59 +0800

    fix(server-client): update appid instead app._id

--- packages/system-client/src/api/application.js
+++ packages/system-client/src/api/application.js
@@ -10,3 +10,17 @@ export function getMyApplications() {
     method: 'get'
   })
 }
+
+/**
+ * 根据 appid 获取应用
+ * @param {string} appid
+ * @returns { Application } 返回应用数据
+ */
+export async function getApplicationByAppid(appid) {
+  const res = await request({
+    url: `/apps/${appid}`,
+    method: 'get'
+  })
+
+  return res
+}
--- packages/system-client/src/api/func.js
+++ packages/system-client/src/api/func.js
@@ -1,6 +1,46 @@
 import request from '@/utils/request'
 
 /**
+ * 分页获取云函数列表
+ * @param {*} query
+ * @param {*} page
+ * @param {*} pageSize
+ */
+export function getFunctions(appid, query, page, pageSize) {
+  return request({
+    url: `/apps/${appid}/function`,
+    method: 'get',
+    params: {
+      ...query,
+      page,
+      limit: pageSize
+    }
+  })
+}
+
+/**
+ * Create a cloud function
+ * @param {string} appid
+ * @param {object} function_data
+ * @returns
+ */
+export function createFunction(appid, function_data) {
+  return request({
+    url: `/apps/${appid}/function/create`,
+    method: 'post',
+    data: function_data
+  })
+}
+
+export function updateFunction(appid, function_data) {
+  return request({
+    url: `/apps/${appid}/function/create`,
+    method: 'post',
+    data: function_data
+  })
+}
+
+/**
  * 运行云函数
  */
 export function launchFunction(functionName, data, debug = false) {
