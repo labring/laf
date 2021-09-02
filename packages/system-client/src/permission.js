a4a3923125a6b5ed722f2c6415760e88ed22696d
maslow <wangfugen@126.com>
2021-09-02 17:53:44 +0800

    fix(server-client): add dynamic router hook

--- packages/system-client/src/permission.js
+++ packages/system-client/src/permission.js
@@ -3,6 +3,9 @@ import NProgress from 'nprogress' // progress bar
 import 'nprogress/nprogress.css' // progress bar style
 import { getToken } from '@/utils/auth' // get token from cookie
 import getPageTitle from '@/utils/get-page-title'
+import store from './store'
+
+import { Message } from 'element-ui'
 
 NProgress.configure({ showSpinner: false }) // NProgress Configuration
 
@@ -23,6 +26,35 @@ router.beforeEach(async(to, from, next) => {
       // if is logged in, redirect to the home page
       next({ path: '/' })
       NProgress.done() // hack: https://github.com/PanJiaChen/vue-element-admin/pull/2939
+    } else if (to.path.startsWith('/app/')) {
+      console.log('router', to.path)
+      if (store.state.app.application) {
+        next()
+      } else {
+        let appid = to.params.appid
+        // parse appid manually while refreshing page
+        if (!appid) {
+          const arr = to.path.split('/')
+          appid = arr[2]
+        }
+
+        // load the application
+        try {
+          await store.dispatch('app/loadCurrentApplication', appid)
+        } catch (error) {
+          console.error(`failed to load application: ${appid}`, error)
+          Message('加载应用信息出错，请重试')
+        }
+
+        const roles = store.state.app.roles
+        const permissions = store.state.app.permissions
+        const accessRoutes = await store.dispatch('permission/generateRoutes', { appid, roles, permissions })
+        router.addRoutes(accessRoutes)
+
+        // hack method to ensure that addRoutes is complete
+        // set the replace: true, so the navigation will not leave a history record
+        next({ ...to, replace: true })
+      }
     } else {
       next()
     }
