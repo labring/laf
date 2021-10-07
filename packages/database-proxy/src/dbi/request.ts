56ba5163899d14fd2c96701eac7875bb67644fa7
maslow <wangfugen@126.com>
2021-10-08 02:14:28 +0800

    fix(db-proxy): remove stale objectid logic

--- packages/database-proxy/src/dbi/request.ts
+++ packages/database-proxy/src/dbi/request.ts
@@ -15,12 +15,10 @@ export class Request implements RequestInterface {
     const accessor = this.accessor
     const params = Proxy.parse(action, data)
     const ret = await accessor.execute(params)
-    // 解决 mongodb _id 对象字符串问题
-    const _data = JSON.parse(JSON.stringify(ret))
 
     return {
       code: 0,
-      data: _data
+      data: ret
     }
   }
 }
