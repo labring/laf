c5e68e8ae5c266b4c3593619810bd36432c69328
maslow <wangfugen@126.com>
2022-01-19 21:27:31 +0800

    feat(gateway): add k8s conf ;

--- packages/gateway/scripts/start.sh
+++ packages/gateway/scripts/start.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+conf_dir=/conf.${SERVICE_DRIVER:-docker}
+
+template_dir="${NGINX_ENVSUBST_TEMPLATE_DIR:-/etc/nginx/templates}"
+
+cp -n ${conf_dir}/system.conf /etc/nginx/templates/system.conf.template || true
+cp -n ${conf_dir}/app.conf /etc/nginx/templates/app.conf.template || true
+cp -n ${conf_dir}/fs-proxy.conf /etc/nginx/templates/fs-proxy.conf.template || true
+
 sh /scripts/auto_envsubst.sh
 
 exec /usr/bin/openresty -g "daemon off;"
