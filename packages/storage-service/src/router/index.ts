b3f313f7665d5bafafd13cc52485c5b52e069107
maslow <wangfugen@126.com>
2022-02-02 00:03:29 +0800

    feat(fs): add /health-check to fs service;

--- packages/storage-service/src/router/index.ts
+++ packages/storage-service/src/router/index.ts
@@ -2,12 +2,19 @@ import { Request, Response, Router } from 'express'
 import { BucketRouter } from './bucket'
 import { FileRouter } from './file'
 import { parseToken } from '../lib/utils'
+import { DatabaseAgent } from '../lib/database'
 
 export const router = Router()
 
+router.get('/health-check', (_req, res) => {
+  if (!DatabaseAgent.db) {
+    return res.status(400).send('no db connection')
+  }
+  return res.status(200).send('ok')
+})
+
 router.use(FileRouter)
 router.use('/sys', checkAuth, BucketRouter)
-
 
 /**
  * Middleware: check auth token to managing buckets
