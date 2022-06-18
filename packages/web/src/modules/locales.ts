529fc3de0fcec988f746b310cd815a8de2ce2bfd
Kaz <54239670+object-kaz@users.noreply.github.com>
2022-06-19 00:08:12 +0800

    feat: language detect and save (#139)

--- packages/web/src/modules/locales.ts
+++ packages/web/src/modules/locales.ts
@@ -1,5 +1,8 @@
 import { createI18n } from 'vue-i18n'
 import messages from '@intlify/vite-plugin-vue-i18n/messages'
+import { getUserLocales } from 'get-user-locale'
+import { intersection } from 'lodash'
+import { useConfigStore } from '~/store'
 
 export const i18n = createI18n({
   locale: 'en',
@@ -18,4 +21,24 @@ export const supportLanguages = [
   },
 ]
 
+export const supportLanguageNames = supportLanguages.map(item => item.name)
+
 export const t = i18n.global.t.bind(i18n.global)
+
+// 检测浏览器中的默认语言
+export const userLocales = getUserLocales()
+
+// 共有的语言
+const commonLanguages = intersection(userLocales, supportLanguageNames)
+
+// 最佳的默认语言
+export const bestDefaultLanguage = commonLanguages.length > 0 ? commonLanguages[0] : 'en'
+
+export function initLanguageModule() {
+  // 同步语言设置
+  const { locale } = useI18n()
+  const config = useConfigStore()
+  watchEffect(() => {
+    locale.value = config.local.language
+  })
+}
