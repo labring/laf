44a1f9d864b5eae928cc21a7289d2d4405d53941
丁振振 <dingzhenzhen@dingzhenzhendeMacBook-Pro.local>
2022-06-22 16:31:20 +0800

    add remote server

--- packages/cli/src/api/user.ts
+++ packages/cli/src/api/user.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export async function loginApi(server:string,obj:Object) {
     // remote server login url
-    const url = `${server}sys-extension-api/func/password-login`;
+    const url = `${server}/sys-extension-api/func/password-login`;
     const result = await axios.post(url,obj)
 
     const response =  result.data
