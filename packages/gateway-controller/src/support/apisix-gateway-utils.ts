aa1662dc637a7e648dcc8c7aa3ab862f3dfda076
october <764213885@qq.com>
2022-07-11 23:56:51 +0800

    feat: add ssl support

--- packages/gateway-controller/src/support/apisix-gateway-utils.ts
+++ packages/gateway-controller/src/support/apisix-gateway-utils.ts
@@ -39,4 +39,40 @@ export class ApiSixHttpUtils {
         return resStatus
     }
 
+    static async putSSL(url: string, id: string, sns: string, cert: string, key: string) {
+        let resStatus = false
+        let data = {
+            cert: cert,
+            key: key,
+            snis: [sns],
+            labels: {'update_time': new Date().getTime() + ''}
+        }
+        await axios.put(url + '/apisix/admin/ssl/' + id, data, {
+            headers: this.headers,
+        })
+            .then(_ => {
+                logger.info('create ssl successful')
+                resStatus = true
+            })
+            .catch(err => {
+                logger.info('create sll failed: ', err)
+            })
+        return resStatus
+    }
+
+    static async deleteSSL(url: string, id: string) {
+        let resStatus = false
+        await axios.delete(url + '/apisix/admin/ssl/' + id, {
+            headers: this.headers,
+        })
+            .then(_ => {
+                logger.info('delete ssl successful')
+                resStatus = true
+            })
+            .catch(err => {
+                logger.info('delete ssl failed: ', err)
+            })
+        return resStatus
+    }
+
 }
