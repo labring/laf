60e48d32e3075fd27097205f1d974f2c50a6fd0c
LeezQ <lizhenq2009@gmail.com>
2022-07-27 16:55:59 +0800

    feat: add filter

--- packages/web/src/main.ts
+++ packages/web/src/main.ts
@@ -8,13 +8,22 @@ import { i18n } from '~/modules/locales'
 import '@unocss/reset/tailwind.css'
 import './styles/main.css'
 import 'uno.css'
+import 'element-plus/dist/index.css'
+
 
 import '~/router/permission'
+import { parseTime } from '~/utils'
 
 const pinia = createPinia()
 
 const app = createApp(App)
 app.use(pinia).use(i18n).use(router).mount('#app')
+
+app.config.globalProperties.$filters = {
+  parseTime(time: string | Date, cFormat: string) {
+    return parseTime(time, cFormat)
+  }
+}
 
 for (const [key, component] of Object.entries(ElementPlusIconsVue))
   app.component(key, component)
