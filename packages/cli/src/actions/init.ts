eb23c359126bd7e4c89c6a541a7612ed27514fe7
zheng <1324652616@qq.com>
2022-07-28 18:14:49 +0800

    fix

--- packages/cli/src/actions/init.ts
+++ packages/cli/src/actions/init.ts
@@ -16,6 +16,7 @@ import * as path from 'node:path'
 
 export async function handleInitAppCommand(appid: string, endpoint: string, oss_endpoint: string) {
     fs.writeFileSync(LAF_CONFIG_FILE, JSON.stringify({ appid: appid, endpoint: endpoint, oss_endpoint: oss_endpoint }))
+    addConfigFromTemplates()
 }
 
 
@@ -29,7 +30,7 @@ export async function handleSyncAppCommand(appid: string) {
     // pull function
     await handlePullListCommand(appid, [])
     // add config file
-    addConfigFromTemplates()
+    //addConfigFromTemplates()
 
 }
 
