efcb52cf37c021d4ca8666882c9aaf4ce63fddbf
october <764213885@qq.com>
2022-08-21 19:40:04 +0800

    feat: opt gateway instance code style

--- packages/gateway-controller/src/index.ts
+++ packages/gateway-controller/src/index.ts
@@ -1,26 +1,26 @@
 import * as express from 'express'
 import Config from './config'
-import { logger } from './support/logger'
-import { DatabaseAgent } from './support/db'
-import { start_scheduler } from './scheduler'
-import { initBaseRoute, initBaseSSL } from "./support/apisix-gateway-init"
+import {logger} from './support/logger'
+import {DatabaseAgent} from './support/db'
+import {start_scheduler} from './scheduler'
+import {initBaseRoute, initBaseSSL} from "./support/apisix-gateway-init"
 
 DatabaseAgent.init(Config.SYS_DB_URI)
 
 const app = express()
 
 app.get('/healthz', (_req, res) => {
-    if (!DatabaseAgent.db) {
-        return res.status(400).send('no db connection')
-    }
-    return res.status(200).send('ok')
+  if (!DatabaseAgent.db) {
+    return res.status(400).send('no db connection')
+  }
+  return res.status(200).send('ok')
 })
 // init base route
 initBaseRoute()
 
 // init base ssl
 if (Config.APP_SERVICE_DEPLOY_URL_SCHEMA === 'https') {
-    initBaseSSL()
+  initBaseSSL()
 }
 
 start_scheduler()
@@ -29,11 +29,11 @@ start_scheduler()
 const server = app.listen(Config.PORT, () => logger.info(`listened on ${Config.PORT}`))
 
 process.on('unhandledRejection', (reason, promise) => {
-    logger.error(`Caught unhandledRejection:`, reason, promise)
+  logger.error(`Caught unhandledRejection:`, reason, promise)
 })
 
 process.on('uncaughtException', err => {
-    logger.error(`Caught uncaughtException:`, err)
+  logger.error(`Caught uncaughtException:`, err)
 })
 
 
@@ -41,12 +41,12 @@ process.on('SIGTERM', gracefullyExit)
 process.on('SIGINT', gracefullyExit)
 
 async function gracefullyExit() {
-    logger.info('exiting: closing db connection')
-    await DatabaseAgent.conn.close()
-    logger.info('exiting: db connection has been closed')
+  logger.info('exiting: closing db connection')
+  await DatabaseAgent.conn.close()
+  logger.info('exiting: db connection has been closed')
 
-    server.close(async () => {
-        logger.info('process gracefully exited!')
-        process.exit(0)
-    })
+  server.close(async () => {
+    logger.info('process gracefully exited!')
+    process.exit(0)
+  })
 }
--- packages/gateway-controller/src/support/route.ts
+++ packages/gateway-controller/src/support/route.ts
@@ -3,31 +3,31 @@ import {CN_ROUTES} from "./constants";
 import {DatabaseAgent} from "./db";
 
 export enum RouteStatus {
-    PREPARED_CREATE = 'prepared_create',
-    CREATED = 'created',
-    PREPARED_PAUSE = 'prepared_pause',
-    PAUSED = 'paused',
-    PREPARED_DELETE = 'prepared_deleted',
-    DELETED = 'deleted',
+  PREPARED_CREATE = 'prepared_create',
+  CREATED = 'created',
+  PREPARED_PAUSE = 'prepared_pause',
+  PAUSED = 'paused',
+  PREPARED_DELETE = 'prepared_deleted',
+  DELETED = 'deleted',
 }
 
 export enum RouteType {
-    APP = 'app',
-    WEBSITE = 'website'
+  APP = 'app',
+  WEBSITE = 'website'
 }
 
 
 export interface IRouteData {
-    _id?: string
-    name: string
-    appid: string
-    type: RouteType
-    website_id: string
-    domain: string[]
-    status: RouteStatus
-    created_by: ObjectId
-    created_at?: Date
-    updated_at?: Date
+  _id?: string
+  name: string
+  appid: string
+  type: RouteType
+  website_id: string
+  domain: string[]
+  status: RouteStatus
+  created_by: ObjectId
+  created_at?: Date
+  updated_at?: Date
 }
 
 /**
@@ -36,12 +36,12 @@ export interface IRouteData {
  * @returns
  */
 export async function getRoutesInStatus(status: RouteStatus) {
-    const db = DatabaseAgent.db
-    const docs = await db.collection<IRouteData>(CN_ROUTES)
-        .find({status: status})
-        .toArray()
+  const db = DatabaseAgent.db
+  const docs = await db.collection<IRouteData>(CN_ROUTES)
+      .find({status: status})
+      .toArray()
 
-    return docs
+  return docs
 }
 
 /**
@@ -52,16 +52,16 @@ export async function getRoutesInStatus(status: RouteStatus) {
  * @returns
  */
 export async function updateRouteStatus(appid: string, from: RouteStatus, to: RouteStatus) {
-    const db = DatabaseAgent.db
-    const r = await db.collection<IRouteData>(CN_ROUTES)
-        .updateOne({
-            appid: appid,
-            status: from,
-        }, {
-            $set: {
-                status: to
-            }
-        })
+  const db = DatabaseAgent.db
+  const r = await db.collection<IRouteData>(CN_ROUTES)
+      .updateOne({
+        appid: appid,
+        status: from,
+      }, {
+        $set: {
+          status: to
+        }
+      })
 
-    return r.modifiedCount
+  return r.modifiedCount
 }
