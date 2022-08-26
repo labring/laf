2c2d4baecf97db1ee888e3e088b81d79e032bf41
moonrailgun <moonrailgun@gmail.com>
2022-08-26 20:01:47 +0800

    fix: fix missing of console.warn/debug/info (#289)

--- packages/app-service/src/support/function-engine/console.ts
+++ packages/app-service/src/support/function-engine/console.ts
@@ -2,21 +2,35 @@ import * as util from 'util'
 import * as dayjs from 'dayjs'
 
 export class FunctionConsole {
-  private _logs: any[] = []
+  private _logs: string[] = []
 
   get logs() {
     return this._logs
   }
 
-  log(...params: any[]) {
+  private _log(...params: any[]) {
     const date = dayjs().format("YYYY/MM/DD HH:mm:ss")
     const r = util.format("[%s] -", date, ...params)
     this._logs.push(r)
   }
 
+  debug(...params: any[]) {
+    this._log(...params)
+  }
+
+  info(...params: any[]) {
+    this._log(...params)
+  }
+
+  log(...params: any[]) {
+    this._log(...params)
+  }
+
+  warn(...params: any[]) {
+    this._log(...params)
+  }
+
   error(...params: any[]) {
-    const date = dayjs().format("YYYY/MM/DD HH:mm:ss")
-    const r = util.format("[%s] -", date, ...params)
-    this._logs.push(r)
+    this._log(...params)
   }
 }
