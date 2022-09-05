de38b4b49d342e8e15ea75105b78827fdd046856
maslow <wangfugen@126.com>
2022-09-05 18:46:10 +0800

    extra runtime mod from app mod

---
+++ controllers/runtime/api/v1/function_types.go
@@ -0,0 +1,99 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// FunctionState Enum
+type FunctionState string
+
+const (
+	FunctionStateDeployed FunctionState = "Deployed"
+)
+
+// FunctionSpec defines the desired state of Function
+type FunctionSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Name of function
+	Name string `json:"name"`
+
+	// Description of function
+	Description string `json:"description"`
+
+	// WebSocket enabled, defaults to false
+	//+kubebuilder:default:=false
+	WebSocketEnabled bool `json:"webSocketEnabled"`
+
+	// Allowed HTTP methods. If empty, http access is disabled.
+	// The value of this field can be any one or a combination of the following:
+	//	GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS, CONNECT, TRACE.
+	// The default values are: ["GET", "POST", "PUT", "DELETE"].
+	AllowedHTTPMethods []string `json:"allowedHTTPMethods"`
+
+	// Code for the function
+	Code string `json:"code"`
+
+	// Compiled code for the function
+	CompiledCode string `json:"compiledCode"`
+
+	// Version of the function
+	Version int64 `json:"version"`
+}
+
+// FunctionStatus defines the observed state of Function
+type FunctionStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// State of the function
+	//+kubebuilder:validation:Enum=Deployed
+	//+kubebuilder:validation:Required
+	//+kubebuilder:default:=Deployed
+	State FunctionState `json:"state"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// Function is the Schema for the functions API
+type Function struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   FunctionSpec   `json:"spec,omitempty"`
+	Status FunctionStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// FunctionList contains a list of Function
+type FunctionList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Function `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Function{}, &FunctionList{})
+}
