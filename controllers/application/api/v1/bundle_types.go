802aa0e025cccc7b1660b918d280d718ebd80698
马斯洛 <wangfugen@126.com>
2022-09-15 19:47:35 +0800

    feat(app): impl creation form crd, update app crds (#317)

--- controllers/application/api/v1/bundle_types.go
+++ controllers/application/api/v1/bundle_types.go
@@ -24,51 +24,51 @@ import (
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
-// SpecificationSpec defines the desired state of Specification
-type SpecificationSpec struct {
+// BundleSpec defines the desired state of Bundle
+type BundleSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// DisplayName for the specification
+	// DisplayName for the bundle
 	DisplayName string `json:"displayName"`
 
-	// Request CPU for the specification
+	// Request CPU for the bundle
 	RequestCPU resource.Quantity `json:"requestCPU"`
 
-	// Request Memory for the specification
+	// Request Memory for the bundle
 	RequestMemory resource.Quantity `json:"requestMemory"`
 
-	// Limit CPU for the specification
+	// Limit CPU for the bundle
 	LimitCPU resource.Quantity `json:"limitCPU"`
 
-	// Limit Memory for the specification
+	// Limit Memory for the bundle
 	LimitMemory resource.Quantity `json:"limitMemory"`
 
-	// Database capacity for the specification
+	// Database capacity for the bundle
 	DatabaseCapacity resource.Quantity `json:"databaseCapacity"`
 
-	// Storage capacity for the specification
+	// Storage capacity for the bundle
 	StorageCapacity resource.Quantity `json:"storageCapacity"`
 
-	// Network Bandwidth Outbound for the specification
+	// Network Bandwidth Outbound for the bundle
 	NetworkBandwidthOutbound resource.Quantity `json:"networkBandwidthOutbound,omitempty"`
 
-	// Network Bandwidth Inbound for the specification
+	// Network Bandwidth Inbound for the bundle
 	NetworkBandwidthInbound resource.Quantity `json:"networkBandwidthInbound,omitempty"`
 
-	// Network Traffic Outbound for the specification
+	// Network Traffic Outbound for the bundle
 	NetworkTrafficOutbound resource.Quantity `json:"networkTrafficOutbound"`
 
-	// Priority for the specification.
-	// The default value is 0, which means that the specification is not currently available for creating new applications.
+	// Priority for the bundle.
+	// The default value is 0, which means that the bundle is not currently available for creating new applications.
 	//+kubebuilder:validation:Minimum=0
 	//+kubebuilder:validation:Maximum=1000
 	//+kubebuilder:validation:Default=10
 	Priority int32 `json:"priority"`
 }
 
-// SpecificationStatus defines the observed state of Specification
-type SpecificationStatus struct {
+// BundleStatus defines the observed state of Bundle
+type BundleStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 }
@@ -76,24 +76,24 @@ type SpecificationStatus struct {
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
 
-// Specification is the Schema for the specifications API
-type Specification struct {
+// Bundle is the Schema for the bundles API
+type Bundle struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec   SpecificationSpec   `json:"spec,omitempty"`
-	Status SpecificationStatus `json:"status,omitempty"`
+	Spec   BundleSpec   `json:"spec,omitempty"`
+	Status BundleStatus `json:"status,omitempty"`
 }
 
 //+kubebuilder:object:root=true
 
-// SpecificationList contains a list of Specification
-type SpecificationList struct {
+// BundleList contains a list of Bundle
+type BundleList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
-	Items           []Specification `json:"items"`
+	Items           []Bundle `json:"items"`
 }
 
 func init() {
-	SchemeBuilder.Register(&Specification{}, &SpecificationList{})
+	SchemeBuilder.Register(&Bundle{}, &BundleList{})
 }
