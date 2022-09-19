6ad851290ad4ce07c18497281eee28eb3d6db364
马斯洛 <wangfugen@126.com>
2022-09-19 15:46:01 +0800

    chore: add vm creation & tests scripts (#326)

---
+++ tests/api/kubernetes.go
@@ -0,0 +1,41 @@
+package api
+
+import (
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	"os"
+	"path/filepath"
+	//
+	// Uncomment to load all auth plugins
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+	//
+	// Or uncomment to load specific auth plugins
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/azure"
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
+)
+
+// GetCurrentKubernetesClient returns a kubernetes client
+func GetCurrentKubernetesClient() *kubernetes.Clientset {
+
+	// get the value of the environment variable KUBE_CONFIG_FILE
+	kubeconfig := os.Getenv("KUBE_CONFIG_FILE")
+	if kubeconfig == "" {
+		// if KUBECONFIG is not set, use the default location
+		kubeconfig = filepath.Join(homedir.HomeDir(), ".kube", "config")
+	}
+
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// create the client
+	client, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	return client
+}
