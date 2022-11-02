df008c940f0baf9d9055b29186eb2746579ce190
LeezQ <lizhenq2009@gmail.com>
2022-11-02 18:56:15 +0800

    refactor: use lingui for i18n

--- packages/next-web/next.config.js
+++ packages/next-web/next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
-const { i18n } = require("./next-i18next.config");
 
 const nextConfig = {
   experimental: {
     newNextLinkBehavior: true,
     // fallbackNodePolyfills: false
   },
-  i18n,
 };
 
 module.exports = nextConfig;
