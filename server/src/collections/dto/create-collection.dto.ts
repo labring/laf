cc4180ca7f7b1635547fcb2ad3f8986ef964313a
maslow <wangfugen@126.com>
2022-11-25 17:07:54 +0800

    feat(server): implement collection creation api.
    Signed-off-by: maslow <wangfugen@126.com>

--- server/src/collections/dto/create-collection.dto.ts
+++ server/src/collections/dto/create-collection.dto.ts
@@ -1,1 +1,14 @@
-export class CreateCollectionDto {}
+import { ApiProperty } from '@nestjs/swagger'
+
+export class CreateCollectionDto {
+  @ApiProperty()
+  name: string
+
+  async validate() {
+    if (!this.name) {
+      return 'Collection name is required'
+    }
+
+    return null
+  }
+}
