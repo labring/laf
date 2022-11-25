ad0c3979cfb69e43f3ffc20397b3270cc40a9c3b
maslow <wangfugen@126.com>
2022-11-25 18:09:03 +0800

    feat(server): impl collection query, update, delete;
    Signed-off-by: maslow <wangfugen@126.com>

--- server/src/collections/collections.controller.ts
+++ server/src/collections/collections.controller.ts
@@ -10,12 +10,13 @@ import {
   UseGuards,
 } from '@nestjs/common'
 import { ApiResponse, ApiTags } from '@nestjs/swagger'
-import { ApplicationAuthGuard } from 'src/applications/application.auth.guard'
-import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
-import { ResponseUtil } from 'src/common/response'
+import { ApplicationAuthGuard } from '../applications/application.auth.guard'
+import { JwtAuthGuard } from '../auth/jwt-auth.guard'
+import { ApiResponseUtil, ResponseUtil } from '../common/response'
 import { CollectionsService } from './collections.service'
 import { CreateCollectionDto } from './dto/create-collection.dto'
 import { UpdateCollectionDto } from './dto/update-collection.dto'
+import { Collection } from './entities/collection.entity'
 
 @ApiTags('Database')
 @Controller('apps/:appid/collections')
@@ -48,26 +49,70 @@ export class CollectionsController {
     return ResponseUtil.error('failed to create collection')
   }
 
+  /**
+   * Get collection list of an application
+   * @param appid
+   * @returns
+   */
+  @ApiResponseUtil(Collection) // QUIRKS: should be array but swagger doesn't support it
+  @UseGuards(JwtAuthGuard, ApplicationAuthGuard)
   @Get()
-  findAll() {
-    return this.collectionService.findAll()
+  async findAll(@Param('appid') appid: string) {
+    const collections = await this.collectionService.findAll(appid)
+    if (collections === null) {
+      return ResponseUtil.error('failed to get collections')
+    }
+    return ResponseUtil.ok(collections)
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.collectionService.findOne(+id)
+  @ApiResponseUtil(Collection)
+  @UseGuards(JwtAuthGuard, ApplicationAuthGuard)
+  @Get(':name')
+  async findOne(@Param('appid') appid: string, @Param('name') name: string) {
+    const res = await this.collectionService.findOne(appid, name)
+    if (!res) {
+      return ResponseUtil.error('failed to get collection')
+    }
+    return ResponseUtil.ok(res)
   }
 
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateCollectionDto: UpdateCollectionDto,
+  /**
+   * Update a collection by its name
+   * @param appid
+   * @param name
+   * @param dto
+   * @returns
+   */
+  @ApiResponse({ type: ResponseUtil })
+  @UseGuards(JwtAuthGuard, ApplicationAuthGuard)
+  @Patch(':name')
+  async update(
+    @Param('appid') appid: string,
+    @Param('name') name: string,
+    @Body() dto: UpdateCollectionDto,
   ) {
-    return this.collectionService.update(+id, updateCollectionDto)
+    const res = await this.collectionService.update(appid, name, dto)
+    if (res === false) {
+      return ResponseUtil.error('failed to update collection')
+    }
+
+    return ResponseUtil.ok(res)
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.collectionService.remove(+id)
+  /**
+   * Delete a collection by its name
+   * @param appid
+   * @param name
+   * @returns
+   */
+  @ApiResponse({ type: ResponseUtil })
+  @UseGuards(JwtAuthGuard, ApplicationAuthGuard)
+  @Delete(':name')
+  async remove(@Param('appid') appid: string, @Param('name') name: string) {
+    const res = await this.collectionService.remove(appid, name)
+    if (res === false) {
+      return ResponseUtil.error('failed to delete collection')
+    }
+    return ResponseUtil.ok(res)
   }
 }
