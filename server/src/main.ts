7ebcaf9708329dab921ccba66d251043ea9613d0
Maslow <wangfugen@126.com>
2022-11-28 23:46:27 +0800

    chore(server): fix to persist bearer auth in swagger ui (#444)
    Signed-off-by: maslow <wangfugen@126.com>

--- server/src/main.ts
+++ server/src/main.ts
@@ -25,7 +25,11 @@ async function bootstrap() {
     )
     .build()
   const document = SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('api', app, document)
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  })
 
   await app.listen(3000)
 }
