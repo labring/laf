71e54359f47553d15c4ea94d0151e6d8b7a84bb5
maslow <wangfugen@126.com>
2022-11-29 17:15:44 +0800

    feat(server): implement bucket update & deletion apis
    Signed-off-by: maslow <wangfugen@126.com>

--- server/src/buckets/buckets.controller.ts
+++ server/src/buckets/buckets.controller.ts
@@ -91,24 +91,64 @@ export class BucketsController {
   @ApiOperation({ summary: 'Get a bucket by name' })
   @UseGuards(JwtAuthGuard, ApplicationAuthGuard)
   @Get(':name')
-  findOne(@Param('appid') appid: string, @Param('name') name: string) {
-    const data = this.bucketsService.findOne(appid, name)
+  async findOne(@Param('appid') appid: string, @Param('name') name: string) {
+    const data = await this.bucketsService.findOne(appid, name)
     if (null === data) {
       throw new HttpException('bucket not found', HttpStatus.NOT_FOUND)
     }
-
     return ResponseUtil.ok(data)
   }
 
-  @ApiOperation({ summary: 'TODO - ⌛️' })
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBucketDto: UpdateBucketDto) {
-    return this.bucketsService.update(+id, updateBucketDto)
+  /**
+   * Update a bucket
+   * @param appid
+   * @param name  bucket name
+   * @param dto
+   * @returns
+   */
+  @ApiOperation({ summary: 'Update a bucket' })
+  @ApiResponseUtil(Bucket)
+  @UseGuards(JwtAuthGuard, ApplicationAuthGuard)
+  @Patch(':name')
+  async update(
+    @Param('appid') appid: string,
+    @Param('name') name: string,
+    @Body() dto: UpdateBucketDto,
+  ) {
+    const bucket = await this.bucketsService.findOne(appid, name)
+    if (!bucket) {
+      throw new HttpException('bucket not found', HttpStatus.NOT_FOUND)
+    }
+
+    // TODO check the storage capacity of the app
+
+    const res = await this.bucketsService.update(bucket, dto)
+    if (null === res) {
+      return ResponseUtil.error('update bucket failed')
+    }
+    return ResponseUtil.ok(res)
   }
 
-  @ApiOperation({ summary: 'TODO - ⌛️' })
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.bucketsService.remove(+id)
+  /**
+   * Delete a bucket
+   * @param appid
+   * @param name bucket name
+   * @returns
+   */
+  @ApiResponseUtil(Bucket)
+  @UseGuards(JwtAuthGuard, ApplicationAuthGuard)
+  @ApiOperation({ summary: 'Delete a bucket' })
+  @Delete(':name')
+  async remove(@Param('appid') appid: string, @Param('name') name: string) {
+    const bucket = await this.bucketsService.findOne(appid, name)
+    if (!bucket) {
+      throw new HttpException('bucket not found', HttpStatus.NOT_FOUND)
+    }
+
+    const res = await this.bucketsService.remove(bucket)
+    if (null === res) {
+      return ResponseUtil.error('delete bucket failed')
+    }
+    return ResponseUtil.ok(res)
   }
 }
--- server/src/buckets/dto/update-bucket.dto.ts
+++ server/src/buckets/dto/update-bucket.dto.ts
@@ -1,4 +1,16 @@
-import { PartialType } from '@nestjs/mapped-types'
-import { CreateBucketDto } from './create-bucket.dto'
+import { ApiProperty } from '@nestjs/swagger'
+import { IsEnum, IsNotEmpty, IsString } from 'class-validator'
+import { BucketPolicy } from '../entities/bucket.entity'
 
-export class UpdateBucketDto extends PartialType(CreateBucketDto) {}
+export class UpdateBucketDto {
+  @ApiProperty({ enum: BucketPolicy })
+  @IsEnum(BucketPolicy)
+  policy: BucketPolicy
+
+  @ApiProperty({
+    description: 'The storage capacity of the bucket: "1Gi", "0.5Gi", "100Gi"',
+  })
+  @IsNotEmpty()
+  @IsString()
+  storage: string
+}
