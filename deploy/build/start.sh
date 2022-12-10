2d41c4af0124a35724eb506aad7b73eae5ef5f71
Maslow <wangfugen@126.com>
2022-12-11 00:37:19 +0800

    build: support domain custom for deploy scripts (#509)
    Signed-off-by: maslow <wangfugen@126.com>

--- deploy/build/start.sh
+++ deploy/build/start.sh
@@ -1,4 +1,14 @@
 kubectl create namespace laf-system || true
 kubectl apply -f manifests/
 
-helm install laf -n laf-system --set minio.persistence.size=1Gi --set mongodb.storage.size=1Gi ./charts/laf
+echo "DOMAIN: $DOMAIN"
+
+helm install laf -n laf-system \
+    --set minio.persistence.size=5Gi \
+    --set mongodb.storage.size=5Gi \
+    --set gateway.http.containerPort=80 \
+    --set gateway.tls.containerPort=443 \
+    --set apisix.kind=DaemonSet \
+    --set apisix.hostNetwork=true \
+    --set global.domain=${DOMAIN} \
+    ./charts/laf
