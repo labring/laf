e3e2e9874a0b55385f4bc2b57eeb02a725366b81
shaoqian.zhang <shaoqian.zhang@appshahe.com>
2022-12-11 21:33:37 +0800

    feat(core): refresh apisix ssl

--- core/controllers/gateway/controllers/domain_controller.go
+++ core/controllers/gateway/controllers/domain_controller.go
@@ -19,16 +19,15 @@ package controllers
 import (
 	"context"
 	"errors"
+	gatewayv1 "github.com/labring/laf/core/controllers/gateway/api/v1"
 	"github.com/labring/laf/core/controllers/gateway/apisix"
 	"github.com/labring/laf/core/pkg/common"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/log"
-
-	gatewayv1 "github.com/labring/laf/core/controllers/gateway/api/v1"
-	corev1 "k8s.io/api/core/v1"
 )
 
 const sslFinalizer = "ssl.gateway.laf.dev"
@@ -76,6 +75,8 @@ func (r *DomainReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctr
 }
 
 func (r *DomainReconciler) apply(ctx context.Context, domain *gatewayv1.Domain) (ctrl.Result, error) {
+	_log := log.FromContext(ctx)
+	_log.Info("apply domain: " + domain.Name)
 
 	if domain.Spec.CertConfigRef == "" {
 		return ctrl.Result{}, nil
--- core/controllers/gateway/main.go
+++ core/controllers/gateway/main.go
@@ -114,6 +114,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Route")
 		os.Exit(1)
 	}
+	if err = (&controllers.SecretReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Route")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
