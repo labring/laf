dc4b91d875a2f38d2188becefe871845f3592bc7
maslow <wangfugen@126.com>
2022-12-13 11:05:03 +0800

    chore(core): clean useless scripts & codes
    Signed-off-by: maslow <wangfugen@126.com>

--- core/tests/api/cluster.go
+++ core/tests/api/cluster.go
@@ -2,26 +2,9 @@ package api
 
 import (
 	"context"
-	"github.com/labring/laf/core/pkg/common"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
-
-func DeleteSystemNamespace() {
-	name := common.GetSystemNamespace()
-	DeleteNamespace(name)
-}
-
-func EnsureDefaultSystemNamespace() *v1.Namespace {
-	name := common.GetSystemNamespace()
-	client := GetDefaultKubernetesClient()
-	ns, err := client.CoreV1().Namespaces().Get(context.TODO(), name, metav1.GetOptions{})
-	if err != nil {
-		return CreateNamespace(client, name)
-	}
-
-	return ns
-}
 
 func EnsureNamespace(name string) *v1.Namespace {
 	client := GetDefaultKubernetesClient()
