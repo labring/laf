3a2669e462cd6961d1e6b8c547aaf11c9f120f5a
maslow <wangfugen@126.com>
2022-12-14 20:45:39 +0800

    refactor(server): add log module (#539)

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -14,6 +14,7 @@ import { ScheduleModule } from '@nestjs/schedule'
 import { DatabaseModule } from './database/database.module'
 import { PrismaService } from './prisma.service'
 import { StorageModule } from './storage/storage.module'
+import { LogModule } from './log/log.module';
 
 @Module({
   imports: [
@@ -32,6 +33,7 @@ import { StorageModule } from './storage/storage.module'
     InstanceModule,
     DatabaseModule,
     StorageModule,
+    LogModule,
   ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
