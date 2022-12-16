cdda44352ce0435efbc6782e291c7caa84c9376d
maslow <wangfugen@126.com>
2022-12-17 05:24:10 +0800

    fix(runtime): update deps installation impl to new design (#553)

--- runtimes/nodejs/src/init.ts
+++ runtimes/nodejs/src/init.ts
@@ -1,28 +1,15 @@
-import { ensureCollectionIndexes, getExtraPackages, initCloudSdkPackage, installPackages, moduleExists } from "./support/init"
-import { logger } from "./support/logger"
-
+import {
+  ensureCollectionIndexes,
+  initCloudSdkPackage,
+  installPackages,
+} from './support/init'
+import { logger } from './support/logger'
 
 async function main() {
   try {
-    const packages = await getExtraPackages()
-    if (!packages.length) {
-      logger.info('no extra packages found')
-    }
-
-    logger.info('packages loaded: ', packages)
-
-    const not_exists = packages.filter(pkg => !moduleExists(pkg.name))
-    if (packages.length && !not_exists.length) {
-      logger.info('no new packages to be installed')
-    }
-
-    if (not_exists.length) {
-      const res = installPackages(packages)
-      logger.info(res)
-    }
+    installPackages()
 
     initCloudSdkPackage()
-
     await ensureCollectionIndexes()
   } catch (error) {
     logger.error(error)
@@ -32,12 +19,11 @@ async function main() {
   return 0
 }
 
-
 main()
-  .then(code => {
+  .then((code) => {
     process.exit(code)
   })
-  .catch(err => {
+  .catch((err) => {
     logger.error(err)
     process.exit(2)
   })
