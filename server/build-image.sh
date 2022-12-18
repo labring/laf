26c73d9a6f6748ffed41dd2612dbbdbaf7380a6b
maslow <wangfugen@126.com>
2022-12-18 19:39:51 +0800

    chore(build): imprv speed of server docker build; fix deploy script problem

--- server/build-image.sh
+++ server/build-image.sh
@@ -3,4 +3,6 @@
 # get version from package.json
 version=$(node -p "require('./package.json').version")
 
-docker buildx build --platform linux/amd64,linux/arm64 --push -t docker.io/lafyun/server:$version -f Dockerfile .
+# docker buildx build --platform linux/amd64,linux/arm64 -t docker.io/lafyun/laf-server:$version -f Dockerfile .
+
+docker buildx build --platform linux/amd64,linux/arm64 --push -t docker.io/lafyun/laf-server:$version -f Dockerfile .
