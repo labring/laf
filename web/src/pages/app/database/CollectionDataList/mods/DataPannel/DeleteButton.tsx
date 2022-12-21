2ef2ba1be3ea66566d0474cffcaa9c2627089501
dingzhenznen <1324652616@qq.com>
2022-12-21 10:45:01 +0800

    fix database data (#564)
    * impl database data CRUD mana

--- web/src/pages/app/database/CollectionDataList/mods/DataPannel/DeleteButton.tsx
+++ web/src/pages/app/database/CollectionDataList/mods/DataPannel/DeleteButton.tsx
@@ -7,8 +7,13 @@ import {
   useDisclosure,
 } from "@chakra-ui/react";
 
-export default function DeleteButton(props: { data: any }) {
+import { useDeleteDataMutation } from "../../../service";
+
+export default function DeleteButton(props: { data: any ,fn: any}) {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const deleteDataMutation = useDeleteDataMutation({onSuccess(data) {
+    props.fn(undefined)
+  }});
 
   return (
     <>
@@ -20,16 +25,27 @@ export default function DeleteButton(props: { data: any }) {
         placement="left"
       >
         <PopoverTrigger>
-          <Button size="xs" px="2" className="mr-2 w-16" onClick={() => {}}>
+          <Button size="xs" px="2" className="mr-2 w-16" onClick={(event) => {
+            event?.stopPropagation();
+          }}>
             Delete
           </Button>
         </PopoverTrigger>
         <PopoverContent p="2" maxWidth={130}>
           <ButtonGroup size="xs">
-            <Button variant="outline" onClick={onClose}>
+            <Button variant="outline" onClick={(event) => {
+              event?.stopPropagation();
+              onClose()
+            }}>
               Cancel
             </Button>
-            <Button colorScheme="red" onClick={() => onClose()}>
+            <Button
+              colorScheme="red"
+              isLoading = {deleteDataMutation.isLoading}
+              onClick={async(event) => {
+              event?.stopPropagation();
+              await deleteDataMutation.mutateAsync(props.data)
+            }}>
               Apply
             </Button>
           </ButtonGroup>
--- web/src/pages/app/database/CollectionDataList/mods/DataPannel/index.tsx
+++ web/src/pages/app/database/CollectionDataList/mods/DataPannel/index.tsx
@@ -1,34 +1,97 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 import SyntaxHighlighter from "react-syntax-highlighter";
-import { AddIcon } from "@chakra-ui/icons";
-import { Button } from "@chakra-ui/react";
+import { AddIcon, Search2Icon } from "@chakra-ui/icons";
+import { Button, HStack, Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import clsx from "clsx";
 
 import JsonEditor from "@/components/Editor/JsonEditor";
+import Pagination from "@/components/Pagination";
+import getPageInfo from "@/utils/getPageInfo";
 
-import { useEntryDataQuery } from "../../../service";
-import useDBMStore from "../../../store";
+import { useAddDataMutation, useEntryDataQuery, useUpdateDataMutation } from "../../../service";
 
 import DeleteButton from "./DeleteButton";
-
-import useDB from "@/hooks/useDB";
-import useGlobalStore from "@/pages/globalStore";
 export default function DataPannel() {
-  const { showSuccess, showError } = useGlobalStore();
-
-  const entryDataQuery = useEntryDataQuery();
-
-  const { currentDB } = useDBMStore();
-
-  const { db } = useDB();
-
   const [currentData, setCurrentData] = useState<any>(undefined);
 
   const [record, setRecord] = useState("");
 
+  type FormData = {
+    _id: string;
+  };
+  const defaultValues = {};
+  const { handleSubmit, register, getValues } = useForm<FormData>({
+    defaultValues,
+  });
+
+  const [queryData, setQueryData] = useState({
+    ...defaultValues,
+  });
+
+  const DEFAULT_LIMIT = 10;
+
+  const entryDataQuery = useEntryDataQuery({ ...queryData, limit: DEFAULT_LIMIT });
+
+  const submit = () => {
+    setQueryData({
+      page: 1,
+      ...getValues(),
+    });
+  };
+
+  const addDataMutation = useAddDataMutation()
+
+  const updateDataMutation = useUpdateDataMutation()
+
+  const handleData = async () => {
+    if (currentData?._id) {
+      const params = JSON.parse(record);
+      await updateDataMutation.mutateAsync(params)
+    } else {
+      const params = JSON.parse(record);
+      await addDataMutation.mutateAsync(params)
+    }
+  }
+
+
   return (
     <>
       <div className="flex pb-4 shadow-sm justify-between items-center">
+        <form
+          onSubmit={(event) => {
+            event?.preventDefault();
+            entryDataQuery.refetch();
+          }}
+        >
+          <div className="flex justify-between my-4">
+            <HStack spacing={2}>
+              <InputGroup width={400}>
+                <InputLeftElement
+                  height={"10"}
+                  pointerEvents="none"
+                  children={<Search2Icon color="gray.300" />}
+                />
+                <Input
+                  borderRadius="4"
+                  placeholder="_id"
+                  bg="white"
+                  {...register("_id")}
+                />
+              </InputGroup>
+              <Button
+                px={9}
+                type={"submit"}
+                colorScheme={"green"}
+                onClick={handleSubmit(submit)}
+                isLoading={entryDataQuery.isFetching}
+              >
+                搜索
+              </Button>
+            </HStack>
+        
+          </div>
+        </form>
         <Button
           colorScheme={"primary"}
           size="sm"
@@ -39,7 +102,17 @@ export default function DataPannel() {
           <AddIcon color="white" className="mr-2" />
           新增记录
         </Button>
-        <span>总数: {entryDataQuery.data?.total}</span>
+        <Pagination
+          values={getPageInfo(entryDataQuery.data as any)}
+          onChange={(values) => {
+              console.log(values)
+              setQueryData({
+                ...values,
+                ...getValues(),
+              });
+            }}
+          />
+        {/* <span>总数: {entryDataQuery.data?.total}</span> */}
       </div>
 
       <div className="absolute top-20 bottom-0 right-2 flex left-4">
@@ -64,7 +137,7 @@ export default function DataPannel() {
                     hidden: currentData?._id !== item._id,
                   })}
                 >
-                  <DeleteButton data={item} />
+                  <DeleteButton data={ item } fn={ setCurrentData } />
                 </div>
 
                 <SyntaxHighlighter language="json" customStyle={{ background: "#fff" }}>
@@ -89,26 +162,30 @@ export default function DataPannel() {
             }}
           >
             <div className="flex justify-between p-2 border-b mb-4">
-              <span>编辑</span>
-              <Button
-                size={"xs"}
-                colorScheme="blue"
-                bgColor={"blue.600"}
-                borderRadius="2"
-                px="4"
-                onClick={async () => {
-                  const params = JSON.parse(record);
-                  const r = await db.collection(currentDB?.name!).add({ ...params });
-                  if (!r.error) {
-                    showSuccess("保存成功");
-                    entryDataQuery.refetch();
-                  } else {
-                    showError("保存失败" + r.error);
-                  }
-                }}
-              >
-                保存
-              </Button>
+              <span>{currentData?._id ? "编辑" : "新增"}</span>
+              <div>
+                <Button
+                  size={"xs"}
+                  colorScheme="blue"
+                  bgColor={"blue.600"}
+                  borderRadius="2"
+                  px="4"
+                  isLoading={currentData?._id?updateDataMutation.isLoading:addDataMutation.isLoading}
+                  onClick={handleData}
+                >
+                  保存
+                </Button>
+                <Button
+                  size={"xs"}
+                  colorScheme="blue"
+                  bgColor={"blue.600"}
+                  borderRadius="2"
+                  ml="3"
+                  onClick={()=>setCurrentData(undefined)}
+                >
+                取消
+                </Button>
+              </div>
             </div>
             <div className=" flex-1" style={{}}>
               <JsonEditor
