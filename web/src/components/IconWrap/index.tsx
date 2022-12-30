2574db5798aec9454e1ca2a9b01a69d237cfb779
allence <lizhenq2009@gmail.com>
2022-12-31 00:11:05 +0800

    feat(web): add tooltip for menu icon

--- web/src/components/IconWrap/index.tsx
+++ web/src/components/IconWrap/index.tsx
@@ -4,12 +4,13 @@ import { Center, Tooltip } from "@chakra-ui/react";
 export default function IconWrap(props: {
   size?: number;
   children: React.ReactNode;
+  placement?: "top" | "bottom" | "left" | "right";
   tooltip?: string | undefined;
   onClick?: () => void;
 }) {
-  const { size = 20, tooltip } = props;
+  const { size = 20, tooltip, placement = "top" } = props;
   return (
-    <Tooltip label={tooltip} placement="top">
+    <Tooltip label={tooltip} placement={placement}>
       <Center
         onClick={props.onClick}
         className="hover:bg-slate-200 rounded inline-block cursor-pointer "
