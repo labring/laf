c79df31f344a075d7c74223f2bf4b4c8c59b8ddc
maslow <wangfugen@126.com>
2023-01-16 06:26:59 +0800

    chore: update deploy scripts

--- deploy/scripts/install-on-linux.sh
+++ deploy/scripts/install-on-linux.sh
@@ -12,20 +12,19 @@ if [ -z "$DOMAIN" ]; then
 fi
 
 # Install Sealos
+
+# if apt installed, use `apt` to install
 if [ -x "$(command -v apt)" ]; then
-  # if apt installed, use `apt` to install
   echo "deb [trusted=yes] https://apt.fury.io/labring/ /" | tee /etc/apt/sources.list.d/labring.list
   apt update
   apt install sealos -y
   
   # fix /etc/hosts overwrite bug in ubuntu while restarting
   sed -i \"/update_etc_hosts/c \\ - ['update_etc_hosts', 'once-per-instance']\" /etc/cloud/cloud.cfg && touch /var/lib/cloud/instance/sem/config_update_etc_hosts
-else
-  echo "apt not installed"
 fi
 
+# if yum installed, use `yum` to install
 if [ -x "$(command -v yum)" ]; then
-  # if yum installed, use `yum` to install
   cat > /etc/yum.repos.d/labring.repo << EOF
 [fury]
 name=labring Yum Repo
@@ -36,8 +35,6 @@ EOF
   # yum update
   yum clean all
   yum install sealos -y
-else
-  echo "yum not installed"
 fi
 
 ARCH=$(arch | sed s/aarch64/arm64/ | sed s/x86_64/amd64/)
@@ -47,14 +44,9 @@ echo "ARCH: $ARCH"
 if [ ! -x "$(command -v sealos)" ]; then
     echo "sealos not installed"
     exit 1
-    # download sealos
-    # wget https://oss.lafyun.com/xaqth3-data/sealos_4.1.4-rc2_linux_${ARCH}.tar.gz  && \
-    #     tar -zxvf sealos_4.1.4-rc2_linux_arm64.tar.gz sealos &&  chmod +x sealos && mv sealos /usr/bin
 fi
 
-
 # install k8s cluster
-# sealos run labring/kubernetes:v1.24.0 labring/flannel:v0.19.0 labring/helm:v3.8.2 --single
 sealos run labring/kubernetes:v1.24.9 labring/flannel:v0.19.0 labring/helm:v3.8.2 --single
 
 # taint master node
@@ -63,16 +55,8 @@ kubectl taint node $NODENAME node-role.kubernetes.io/master-
 kubectl taint node $NODENAME node-role.kubernetes.io/control-plane-
 
 # install required components
-# sealos run labring/coredns:v0.0.1
 sealos run labring/openebs:v1.9.0
 sealos run labring/cert-manager:v1.8.0
 
 
 sealos run --env DOMAIN=$DOMAIN --env DB_PV_SIZE=3Gi --env OSS_PV_SIZE=3Gi lafyun/laf:latest
-
-# Optional installations
-#arch=$(arch | sed s/aarch64/arm64/ | sed s/x86_64/amd64/)
-#vm_root_exec echo "download buildah in https://github.com/labring/cluster-image/releases/download/depend/buildah.linux.${arch}"
-#vm_root_exec wget -qO "buildah" "https://github.com/labring/cluster-image/releases/download/depend/buildah.linux.${arch}"
-#vm_root_exec chmod a+x buildah
-#vm_root_exec mv buildah /usr/bin
