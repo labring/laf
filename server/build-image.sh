73a3270e5e555980309df7d036527953363d02f2
maslow <wangfugen@126.com>
2023-01-15 21:33:23 +0800

    chore(ci): fix docker file issue of server (#630)
    * debug server dockerfile
    
    * chore: fix server docker file error
    
    * chore: fix dockerfile

--- server/build-image.sh
+++ server/build-image.sh
@@ -1,8 +1,7 @@
 # Warning: just for development phase, will be move to github action in future.
 
-# get version from package.json
-version=$(node -p "require('./package.json').version")
 
-# docker buildx build --platform linux/amd64,linux/arm64 -t docker.io/lafyun/laf-server:$version -f Dockerfile .
 
-docker buildx build --platform linux/amd64,linux/arm64 --push -t docker.io/lafyun/laf-server:$version -f Dockerfile .
+docker buildx build --platform linux/amd64,linux/arm64 --push -t docker.io/lafyun/laf-server:dev -f Dockerfile .
+
+# docker buildx build --platform linux/amd64,linux/arm64 --push -t ghcr.io/labring/laf-server:dev -f Dockerfile .
