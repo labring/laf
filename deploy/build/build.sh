d0019b52360ce3610181ddc554a149fc8e253ff9
maslow <wangfugen@126.com>
2023-01-19 03:55:12 +0800

    chore: remove gateway crd & core module

--- deploy/build/build.sh
+++
@@ -1,6 +0,0 @@
-sh ./pre-build.sh
-
-IMAGE=docker.io/lafyun/laf
-VERSION=dev
-sealos build -t $IMAGE:$VERSION-arm64 --platform linux/arm64 -f Kubefile  .
-sealos build -t $IMAGE:$VERSION-amd64 --platform linux/amd64 -f Kubefile  .
