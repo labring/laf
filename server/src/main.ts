249e622f8e78f288863f6895682d25f24a474117
maslow <wangfugen@126.com>
2023-01-19 18:54:45 +0800

    chore(deploy): remove gateway crd config (#657)

--- server/src/main.ts
+++ server/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: '*',
     methods: HTTP_METHODS,
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
