d67d41776b566d2ecd05a382dd5642e8ee420a14
kongwy229 <1419906205@qq.com>
2023-02-01 21:56:53 +0800

    feat(web): fix function tags

--- web/src/pages/app/functions/mods/EditorPanel/FunctionDetailPopOver.tsx
+++ web/src/pages/app/functions/mods/EditorPanel/FunctionDetailPopOver.tsx
@@ -1,5 +1,13 @@
 import { BiMessageSquareDetail } from "react-icons/bi";
-import { Popover, PopoverBody, PopoverContent, PopoverTrigger, Portal } from "@chakra-ui/react";
+import {
+  Popover,
+  PopoverBody,
+  PopoverContent,
+  PopoverTrigger,
+  Portal,
+  Tag,
+  TagLabel,
+} from "@chakra-ui/react";
 
 import FileTypeIcon from "@/components/FileTypeIcon";
 import IconWrap from "@/components/IconWrap";
@@ -25,9 +33,16 @@ const FunctionDetailPopOver = () => {
               <span className="align-middle">{currentFunction.name}</span>{" "}
               <FileTypeIcon type="ts" />
             </h2>
-            <div className="text-second">
+            <div className="text-grayIron-600">
               <span>更新于 {formatDate(currentFunction.updatedAt)}</span>
               <p className="mt-2">{currentFunction.desc}</p>
+            </div>
+            <div className="w-full mt-2 flex flex-wrap justify-start items-center">
+              {currentFunction.tags.map((item: string) => (
+                <Tag className="mr-2 mb-2 cursor-pointer" key={item} variant="inputTag">
+                  <TagLabel>{item}</TagLabel>
+                </Tag>
+              ))}
             </div>
           </PopoverBody>
         </PopoverContent>
