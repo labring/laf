00b6a52ac919887274dc06713fd30f635c629ea8
kangert <kangert@qq.com>
2023-02-07 09:43:59 +0800

    feat(web): optimize toast

--- web/src/hooks/useCustomToast.ts
+++ web/src/hooks/useCustomToast.ts
@@ -1,25 +1,30 @@
 import React from "react";
 import { useToast } from "@chakra-ui/react";
+import { throttle as useThrottle } from "lodash";
 
 function useCustomToast() {
   const toast = useToast();
 
   const showSuccess = (text: string | React.ReactNode) => {
-    toast({
-      position: "top",
-      title: text,
-      status: "success",
-      duration: 1000,
-    });
+    useThrottle(() => {
+      toast({
+        position: "top",
+        title: text,
+        status: "success",
+        duration: 1000,
+      });
+    }, 500);
   };
 
   const showError = (text: string | React.ReactNode) => {
-    toast({
-      position: "top",
-      title: text,
-      status: "error",
-      duration: 1000,
-    });
+    useThrottle(() => {
+      toast({
+        position: "top",
+        title: text,
+        status: "error",
+        duration: 1000,
+      });
+    }, 500);
   };
   return {
     showSuccess,
