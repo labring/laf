c5482338f11f92be745fc25723f623e710d32eb9
左风 <595563214@qq.com>
2023-02-15 17:22:52 +0800

    fix(web): support tls conf in #767

--- web/src/pages/app/storages/mods/FileList/index.tsx
+++ web/src/pages/app/storages/mods/FileList/index.tsx
@@ -54,13 +54,15 @@ export default function FileList() {
       changeDirectory(file);
       return;
     }
-    console.log(currentApp);
-    const fileUrl =
-      bucketType === "private"
-        ? getFileUrl(bucketName!, file.Key)
-        : `${(address && address[0]) || "http"}//${bucketName}.${(address && address[1]) || ""}/${
-            file.Key
-          }`;
+    const protocol = address && address[0];
+    const ossUrl = address && address[1];
+    let fileUrl = "";
+
+    if (bucketType === "private") {
+      fileUrl = getFileUrl(bucketName!, file.Key);
+    } else {
+      fileUrl = protocol + "//" + bucketName + "." + ossUrl + "/" + file.Key;
+    }
 
     window.open(fileUrl, "_blank");
   };
