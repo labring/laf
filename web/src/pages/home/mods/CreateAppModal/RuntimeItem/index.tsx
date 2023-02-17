f71ba10ebf472e14de382698b542da57e6fb5221
allence <lizhenq2009@gmail.com>
2023-02-17 23:47:04 +0800

    feat(web): add more app bundle
    (cherry picked from commit d9fe089cf0af6304772156601ff286895b88acb6)
    (cherry picked from commit d76892296ffee76b1146a4e3744de38834e40a2b)

---
+++ web/src/pages/home/mods/CreateAppModal/RuntimeItem/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { CheckIcon } from "@chakra-ui/icons";
+
+import { TBundle } from "@/apis/typing";
+
+export default function RuntimeItem(props: { bundle?: TBundle }) {
+  return (
+    <div className="p-2 flex items-center bg-primary-100 text-primary-500 font-semibold rounded border-primary-300 border">
+      <CheckIcon className="mr-2" />
+      <img src="/node.svg" alt="" className="mr-1" /> - latest
+    </div>
+  );
+}
