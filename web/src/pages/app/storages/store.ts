a597aa150d8b5b33e56795c57d4c10a28ef2959a
kongwy229 <1419906205@qq.com>
2023-02-22 15:00:13 +0800

    feat(web): add user information

--- web/src/pages/app/storages/store.ts
+++ web/src/pages/app/storages/store.ts
@@ -1,4 +1,4 @@
-import { create } from "zustand";
+import create from "zustand";
 import { devtools } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 
--- web/src/pages/customSetting.ts
+++ web/src/pages/customSetting.ts
@@ -1,5 +1,5 @@
 import React from "react";
-import { create } from "zustand";
+import create from "zustand";
 import { devtools, persist } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 
