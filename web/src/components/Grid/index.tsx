7d943c6a0d9b58dea7aaf8ca37c8f14b523f8fe2
kongwy229 <1419906205@qq.com>
2023-03-04 18:38:58 +0800

    feat(web): add resize & prompt before leave function page

--- web/src/components/Grid/index.tsx
+++ web/src/components/Grid/index.tsx
@@ -1,29 +1,35 @@
 import React from "react";
+import { forwardRef } from "react";
 import clsx from "clsx";
-
 function Grid() {
   return <div>Grid</div>;
 }
 
-function Row(props: {
-  className?: string;
-  style?: React.CSSProperties;
-  children: React.ReactNode;
-}) {
-  const { className, style } = props;
-  return (
-    <div
-      className={clsx(
-        "flex space-x-2 w-full",
-        style?.height ? "flex-none" : "flex-grow",
-        className,
-      )}
-      style={style}
-    >
-      {props.children}
-    </div>
-  );
-}
+const Row = forwardRef(
+  (
+    props: {
+      className?: string;
+      style?: React.CSSProperties;
+      children: React.ReactNode;
+    },
+    ref: any,
+  ) => {
+    const { className, style } = props;
+    return (
+      <div
+        ref={ref}
+        className={clsx(
+          "flex w-full overflow-hidden",
+          style?.height ? "flex-none" : "flex-grow",
+          className,
+        )}
+        style={style}
+      >
+        {props.children}
+      </div>
+    );
+  },
+);
 
 function Col(props: {
   className?: string;
@@ -34,7 +40,7 @@ function Col(props: {
   return (
     <div
       className={clsx(
-        "flex flex-col space-y-2 h-full",
+        "flex flex-col h-full overflow-x-hidden",
         style?.width ? "flex-none" : "flex-grow flex-1 overflow-hidden",
         className,
       )}
--- web/src/pages/app/functions/index.tsx
+++ web/src/pages/app/functions/index.tsx
@@ -2,8 +2,11 @@
  * cloud functions index page
  ***************************/
 
+import { useRef } from "react";
+
 import Content from "@/components/Content";
 import { Col, Row } from "@/components/Grid";
+import Resize from "@/components/Resize";
 
 import BottomPanel from "./mods/BottomPanel";
 import ConsolePanel from "./mods/ConsolePanel";
@@ -13,28 +16,50 @@ import EditorPanel from "./mods/EditorPanel";
 import FunctionPanel from "./mods/FunctionPanel";
 
 import useCustomSettingStore from "@/pages/customSetting";
-
 function FunctionPage() {
+  const containerRef = useRef<HTMLDivElement>(null);
   const functionPageConfig = useCustomSettingStore((store) => store.layoutInfo.functionPage);
   return (
     <Content>
-      <Row className="overflow-hidden">
+      <Row ref={containerRef}>
         <Col {...functionPageConfig.SideBar}>
           <FunctionPanel />
+          <Resize
+            type="y"
+            pageId="functionPage"
+            panelId="DependencePanel"
+            reverse
+            containerRef={containerRef}
+          />
           <Row {...functionPageConfig.DependencePanel}>
             <DependencePanel />
           </Row>
         </Col>
-        <Col className="overflow-hidden">
-          <Row className="overflow-hidden">
+        <Resize type="x" pageId="functionPage" panelId="SideBar" containerRef={containerRef} />
+        <Col>
+          <Row>
             <EditorPanel />
           </Row>
+          <Resize
+            type="y"
+            pageId="functionPage"
+            panelId="ConsolePanel"
+            reverse
+            containerRef={containerRef}
+          />
           <Row {...functionPageConfig.ConsolePanel}>
             <ConsolePanel />
           </Row>
         </Col>
+        <Resize
+          type="x"
+          pageId="functionPage"
+          panelId="RightPanel"
+          reverse
+          containerRef={containerRef}
+        />
         <Col {...functionPageConfig.RightPanel}>
-          <DebugPanel />
+          <DebugPanel containerRef={containerRef} />
         </Col>
       </Row>
       <Row {...functionPageConfig.Bottom}>
