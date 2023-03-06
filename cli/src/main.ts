409312a8140f3721b5bf433de8edd1e529d26e20
skyoct <764213885@qq.com>
2023-03-06 14:40:36 +0800

    feat(cli): impl website command (#854)
    * feat: impl website command

--- cli/src/main.ts
+++ cli/src/main.ts
@@ -4,10 +4,10 @@ import { Command } from 'commander'
 import { command as applicationCommand } from './command/application/'
 import { command as functionCommand } from './command/function/'
 import { command as dependencyCommand } from './command/dependency/'
-
 import { loginCommand, logoutCommand } from './command/auth'
-import { bucketCommand } from './command/storage'
-import { policyCommand } from './command/policy'
+import { command as storageCommand } from './command/storage'
+import { command as policyCommand } from './command/policy'
+import { command as websiteCommand } from './command/website'
 
 const program = new Command()
 program.option('-v, --version', 'output version').action((options) => {
@@ -23,8 +23,9 @@ program.addCommand(loginCommand())
 program.addCommand(logoutCommand())
 program.addCommand(applicationCommand())
 program.addCommand(functionCommand())
-program.addCommand(bucketCommand())
+program.addCommand(storageCommand())
 program.addCommand(dependencyCommand())
 program.addCommand(policyCommand())
+program.addCommand(websiteCommand())
 
 program.parse(process.argv)
