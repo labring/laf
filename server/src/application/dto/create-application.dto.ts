c3b39d9dbb5d0b9293b6bb1df46aadfdac05117b
maslow <wangfugen@126.com>
2023-03-07 18:32:26 +0800

    chore(server): replace region to regionId in app creating api (#867)

--- server/src/application/dto/create-application.dto.ts
+++ server/src/application/dto/create-application.dto.ts
@@ -23,7 +23,8 @@ export class CreateApplicationDto {
 
   @ApiProperty()
   @IsNotEmpty()
-  region: string
+  @IsString()
+  regionId: string
 
   @ApiProperty()
   @IsNotEmpty()
@@ -32,7 +33,8 @@ export class CreateApplicationDto {
 
   @ApiProperty()
   @IsNotEmpty()
-  runtimeName: string
+  @IsString()
+  runtimeId: string
 
   validate(): string | null {
     return null
