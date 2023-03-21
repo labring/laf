89dfc1dea923607aabbd7ea66605e89dd83741b7
walle233 <2310005663@qq.com>
2023-03-21 23:49:44 +0800

    styles: sign in page ui bg fix && add welcome text

--- web/src/layouts/Auth/index.tsx
+++ web/src/layouts/Auth/index.tsx
@@ -6,6 +6,10 @@ import styles from "./index.module.scss";
 export default function LoginReg() {
   return (
     <div className={clsx(styles.container)}>
+      <div className={styles.welcomeWrap}>
+        <div className={styles.welcomeText}>Welcome to laf !</div>
+        <div className={styles.sloganText}>life is short, you need laf.</div>
+      </div>
       <Outlet />
     </div>
   );
--- web/src/layouts/LoginReg.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from "react-router-dom";
-
-export default function LoginReg() {
-  return (
-    <div className="pb-16">
-      <Outlet />
-    </div>
-  );
-}
