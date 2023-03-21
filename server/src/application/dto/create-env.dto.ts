48bee67b10485f6d982543f95f76a61be651d0a7
maslow <wangfugen@126.com>
2023-03-22 06:40:11 +0800

    fix(server): add regex for environment name
    Signed-off-by: maslow <wangfugen@126.com>

--- server/src/application/dto/create-env.dto.ts
+++ server/src/application/dto/create-env.dto.ts
@@ -1,11 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger'
-import { IsNotEmpty, IsString, Length } from 'class-validator'
+import { IsNotEmpty, IsString, Length, Matches } from 'class-validator'
 
 export class CreateEnvironmentDto {
   @ApiProperty()
   @IsNotEmpty()
   @IsString()
   @Length(1, 64)
+  @Matches(/^[a-zA-Z_][a-zA-Z0-9_]*$/)
   name: string
 
   @ApiProperty()
