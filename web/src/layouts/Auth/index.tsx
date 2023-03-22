6998cb45a972419cae82b186eaafe1a32a42d375
walle233 <2310005663@qq.com>
2023-03-22 19:02:50 +0800

    fix: login text color to theme color

--- web/src/layouts/Auth/index.tsx
+++ web/src/layouts/Auth/index.tsx
@@ -7,8 +7,10 @@ export default function LoginReg() {
   return (
     <div className={clsx(styles.container)}>
       <div className={styles.welcomeWrap}>
-        <div className={styles.welcomeText}>Welcome to laf !</div>
-        <div className={styles.sloganText}>life is short, you need laf.</div>
+        <div className={clsx(styles.welcomeText, ["text-primary-600"])}>Welcome to laf !</div>
+        <div className={clsx(styles.sloganText, ["text-grayModern-500"])}>
+          life is short, you need laf.
+        </div>
       </div>
       <Outlet />
     </div>
