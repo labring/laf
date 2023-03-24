e2a88320a297de8b9887e1cbbbc6f0465751525f
walle233 <2310005663@qq.com>
2023-03-24 15:51:12 +0800

    fix: auth css

--- web/src/layouts/Auth/index.tsx
+++ web/src/layouts/Auth/index.tsx
@@ -1,16 +1,13 @@
 import { Outlet } from "react-router-dom";
-import clsx from "clsx";
 
 import styles from "./index.module.scss";
 
 export default function LoginReg() {
   return (
-    <div className={clsx(styles.container)}>
-      <div className={styles.welcomeWrap}>
-        <div className={clsx(styles.welcomeText, ["text-primary-600"])}>Welcome to laf !</div>
-        <div className={clsx(styles.sloganText, ["text-grayModern-500"])}>
-          life is short, you need laf.
-        </div>
+    <div className={styles.container}>
+      <div className="absolute top-[120px] left-[120px]">
+        <div className="text-primary-600 text-[36px]">Welcome to laf !</div>
+        <div className="text-grayModern-500 text-[20px]">life is short, you need laf.</div>
       </div>
       <Outlet />
     </div>
