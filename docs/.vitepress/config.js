000536f99fe9f7383d5b908943f4bc2fcf3aaf89
zuofeng <595563214@qq.com>
2023-04-13 15:59:16 +0800

    doc: delete editLink

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -203,10 +203,10 @@ export default defineConfig({
       message: "Apache License V2.0",
       copyright: "Copyright © 2021-present labring/laf",
     },
-    editLink: {
-      pattern: "https://github.com/labring/laf/edit/main/docs/:path",
-      text: "在 GitHub 上编辑此页",
-    },
+    // editLink: {
+    //   pattern: "https://github.com/labring/laf/edit/main/docs/:path",
+    //   text: "在 GitHub 上编辑此页",
+    // },
     lastUpdated: "更新于",
     nav: NavConfig,
     socialLinks: [{ icon: "github", link: "https://github.com/labring/laf" }],
