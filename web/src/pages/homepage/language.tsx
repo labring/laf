fe08577243702f169d9ec4a92d3adf06cfaf8ec5
heheer <1239331448@qq.com>
2023-04-18 17:02:47 +0800

    homepage_darkmode

--- web/src/pages/homepage/language.tsx
+++ web/src/pages/homepage/language.tsx
@@ -1,9 +1,12 @@
 import { useTranslation } from "react-i18next";
+import { useColorMode } from "@chakra-ui/react";
 
 type Props = {};
 
 const Language = (props: Props) => {
   const { i18n } = useTranslation();
+  const { colorMode } = useColorMode();
+  const darkMode = colorMode === "dark";
 
   return (
     <div className="mr-4">
@@ -15,7 +18,11 @@ const Language = (props: Props) => {
           i18n.changeLanguage(i18n.language === "en" ? "zh" : "en");
         }}
       >
-        <img className="pr-2" src="/homepage/language.svg" alt="language" />
+        <img
+          className="pr-2"
+          src={darkMode ? "/homepage/dark/language.svg" : "/homepage/language.svg"}
+          alt="language"
+        />
         <p>{i18n.language === "en" ? "中文" : "English"}</p>
       </a>
     </div>
