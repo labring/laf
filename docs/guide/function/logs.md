74c06bfc8a15997deb74de4647cdbd661c479dff
NightWhite <nightwhite634@gmail.com>
2023-04-21 01:06:13 +0800

    doc: optimize cloud function document && add document search function

---
+++ docs/guide/function/logs.md
@@ -0,0 +1,41 @@
+---
+title: 云函数历史日志
+---
+
+# {{ $frontmatter.title }}
+
+云函数的全部历史日志，全部都自动保存到了日志板块中，日志会保留7天
+
+可根据请求ID `requestId` 和云函数名筛选
+
+![function-log](/doc-images/function-log.png)
+
+1、点击日志，切换到日志板块
+
+2、可根据 `requestId` 和云函数名搜索指定日志
+
+3、点击单个日志，可查看详细内容
+
+## 手动清理日志
+
+Laf云函数的运行日志都在一个隐藏的集合中：`__function_logs__`
+
+所以我们可以通过云函数操作数据库的方法，清理日志
+
+以下是清理全部日志的云函数写法：
+
+::: danger
+以下操作会删除全部历史日志，请谨慎操作
+:::
+
+```ts
+import cloud from '@lafjs/cloud'
+
+export async function main(ctx: FunctionContext) {
+  console.log('Hello World')
+  // 数据库,删除全部日志
+  const db = cloud.database();
+  const res = await db.collection('__function_logs__').remove({multi:true})
+  console.log(res)
+}
+```
--- docs/vite.config.js
+++ docs/vite.config.js
@@ -1,11 +1,23 @@
-// vite.config.js
-export default {
-  // config options
+//vite.config.js
+import { SearchPlugin } from "vitepress-plugin-search";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "./",
   server: {
-    fs: {
-      // Allow serving files from one level up to the project root
-      allow: ["."],
-      strict: false,
-    },
+    port: 5173,
+    hmr: false,
+    disableHostCheck: true,
   },
-};
+  plugins: [
+    SearchPlugin({
+      encode: false,
+      tokenize: "full",
+      previewLength: 62,
+      buttonLabel: "Search",
+      placeholder: "Search docs",
+      cache: true,
+      context: false,
+    }),
+  ],
+});
