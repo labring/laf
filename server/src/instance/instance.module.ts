7e3fbf6478c61e12d2f66b2842c9b20dbd92df84
maslow <wangfugen@126.com>
2023-05-17 08:46:28 +0000

    remove prisma in instance module

--- server/src/instance/instance.module.ts
+++ server/src/instance/instance.module.ts
@@ -4,9 +4,10 @@ import { InstanceTaskService } from './instance-task.service'
 import { StorageModule } from '../storage/storage.module'
 import { DatabaseModule } from '../database/database.module'
 import { TriggerModule } from 'src/trigger/trigger.module'
+import { ApplicationModule } from 'src/application/application.module'
 
 @Module({
-  imports: [StorageModule, DatabaseModule, TriggerModule],
+  imports: [StorageModule, DatabaseModule, TriggerModule, ApplicationModule],
   providers: [InstanceService, InstanceTaskService],
 })
 export class InstanceModule {}
