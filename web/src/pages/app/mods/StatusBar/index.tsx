7d1eccdab6875f56e045a665fb180bee871e27c7
heheer <1239331448@qq.com>
2023-06-16 19:55:55 +0800

    fix(web): delete console

--- web/src/pages/app/mods/StatusBar/index.tsx
+++ web/src/pages/app/mods/StatusBar/index.tsx
@@ -14,7 +14,6 @@ import StatusBadge from "@/pages/home/mods/StatusBadge";
 function StatusBar() {
   const { t } = useTranslation();
   const { currentApp } = useGlobalStore((state) => state);
-  console.log(currentApp);
 
   return (
     <Panel className="!flex-row justify-between">
