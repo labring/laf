be02bf70147ee1082b5b0d477ee1c5fb2bc5fe6e
heheer <71265218+newfish-cmyk@users.noreply.github.com>
2023-06-17 00:03:29 +0800

    fix(web): hide bonus when no bonus (#1267)
    * fix(web): hide bonus when no bonus
    
    * fix(web): fix amount display
    
    * fix(web): fix can't open charge modal

--- web/src/components/ChargeButton/index.tsx
+++ web/src/components/ChargeButton/index.tsx
@@ -35,7 +35,7 @@ export default function ChargeButton(props: { amount?: number; children: React.R
   const darkMode = useColorMode().colorMode === "dark";
   const { isOpen, onOpen, onClose } = useDisclosure();
 
-  const [amount, setAmount] = React.useState<number>();
+  const [amount, setAmount] = React.useState<number>(0);
   const [bonus, setBonus] = React.useState<[{ amount: number; reward: number }]>();
   const inputRef = useRef<HTMLInputElement>(null);
 
@@ -60,9 +60,10 @@ export default function ChargeButton(props: { amount?: number; children: React.R
       refetchInterval: phaseStatus === "Pending" && isOpen ? 1000 : false,
       onSuccess: (res) => {
         setPhaseStatus(res?.data?.phase);
-        if (res?.data?.phase === "Paid") {
+        if (res?.data?.phase === "Paid" && phaseStatus !== "Paid") {
           accountRefetch();
           onClose();
+          setPhaseStatus(res?.data?.phase);
         }
       },
     },
@@ -75,14 +76,15 @@ export default function ChargeButton(props: { amount?: number; children: React.R
       enabled: isOpen,
       onSuccess: (res) => {
         setBonus(res.data);
+        console.log(res.data);
       },
     },
   );
 
   const matchBonus = (amount: number) => {
-    const index = (bonus || []).findIndex((item) => item.amount > (amount || 0));
+    const index = (bonus || []).findIndex((item) => item.amount > amount);
     const matchedItem = index === -1 ? bonus?.[bonus?.length - 1] : bonus?.[index - 1];
-    return matchedItem?.reward;
+    return matchedItem?.reward && matchedItem?.reward / 100;
   };
 
   return (
@@ -101,35 +103,35 @@ export default function ChargeButton(props: { amount?: number; children: React.R
                   ¥ {formatPrice(accountRes?.data?.balance)}
                 </span>
               </div>
-              <p className="mb-4 text-second">{t("Recharge amount")}</p>
+              {bonus && <p className="mb-4 text-second">{t("Recharge amount")}</p>}
               <div className="mb-5 grid grid-cols-3 gap-4">
                 {(bonus || []).map((item) => (
                   <div className="relative">
                     {item.reward && (
                       <span className="absolute left-20 top-1 z-50 whitespace-nowrap rounded-full rounded-bl-none bg-purple-200 px-4 py-[1.5px] text-[12px] text-purple-600">
-                        {t("application.bonus")} ¥{item.reward}
+                        {t("application.bonus")} ¥{item.reward / 100}
                       </span>
                     )}
                     <Button
                       className={clsx(
                         "w-full !rounded-md !border-2 bg-gray-100 py-10 !text-[24px]",
-                        item.amount === amount
+                        item.amount === amount * 100
                           ? "!border-primary-400 !text-primary-600"
                           : "!border-transparent",
                       )}
                       variant={"outline"}
                       key={item.amount}
                       onClick={() => {
-                        setAmount(item.amount);
+                        setAmount(item.amount / 100);
                       }}
                     >
-                      ¥{item.amount}
+                      ¥{item.amount / 100}
                     </Button>
                   </div>
                 ))}
               </div>
               <InputGroup className="flex items-center pb-5">
-                <div className="w-3/12 text-lg text-second">{t("application.Recharge")}</div>
+                <div className="w-20 text-lg text-second">{t("application.Recharge")}</div>
                 <InputLeftAddon className="!px-0 !pl-3" children="¥" />
                 <Input
                   ref={inputRef}
@@ -140,14 +142,18 @@ export default function ChargeButton(props: { amount?: number; children: React.R
                     if (isNumber(value) && !isNaN(value)) {
                       setAmount(value);
                     } else {
-                      if (inputRef.current) inputRef.current.value = String(amount || "");
+                      if (inputRef.current) inputRef.current.value = String(amount);
                     }
                   }}
                 />
-                <span className="ml-3 whitespace-nowrap rounded-full rounded-bl-none bg-purple-200 px-2 py-[1.5px] text-[12px] text-purple-600">
-                  赠
-                </span>
-                <span className="pl-1 font-semibold">¥{amount && matchBonus(amount)}</span>
+                {bonus && (
+                  <div>
+                    <span className="ml-3 whitespace-nowrap rounded-full rounded-bl-none bg-purple-200 px-2 py-[1.5px] text-[12px] text-purple-600">
+                      {t("application.bonus")}
+                    </span>
+                    <span className="pl-1 font-semibold">¥{matchBonus(amount * 100)}</span>
+                  </div>
+                )}
               </InputGroup>
               {/* <span className="flex items-center cursor-pointer text-[#1D8CDC] pb-6">
                 <InfoOutlineIcon className="mr-2" />
@@ -169,7 +175,7 @@ export default function ChargeButton(props: { amount?: number; children: React.R
               </Button>
             </div>
 
-            {createOrderRes?.data?.result?.code_url && (
+            {createOrderRes?.data?.result?.code_url && phaseStatus !== "Paid" && (
               <div className="mt-4 flex flex-col items-center text-xl ">
                 <h2 className="mb-2">{t("Scan with WeChat")}</h2>
                 <QRCodeSVG
