442445afe93f34f353a96d7677802dc0c6f130d3
0fatal <2816813070@qq.com>
2023-06-26 03:00:10 +0000

    feat(web): support pwa

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,7 @@
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig, loadEnv } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import pluginRewriteAll from "vite-plugin-rewrite-all";
 
 // https://vitejs.dev/config/
@@ -17,7 +18,31 @@ export default defineConfig(({ command, mode }) => {
         },
       },
     },
-    plugins: [react(), pluginRewriteAll()],
+    plugins: [
+      react(),
+      pluginRewriteAll(),
+      VitePWA({
+        includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.png"],
+        manifest: {
+          name: "laf 云开发",
+          short_name: "Laf",
+          description: "life is short, you need laf:)",
+          theme_color: "#eef0f2",
+          icons: [
+            {
+              src: "pwa-192x192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "pwa-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+            },
+          ],
+        },
+      }),
+    ],
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
