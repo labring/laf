347ccb40208cdc7cf15b82c3be21001113a52fd1
0fatal <72899968+0fatal@users.noreply.github.com>
2023-06-29 17:27:55 +0800

    fix(server): don't add history when debug function (#1334)

--- server/src/function/function.service.ts
+++ server/src/function/function.service.ts
@@ -97,8 +97,11 @@ export class FunctionService {
     )
 
     const fn = await this.findOne(func.appid, func.name)
-    await this.addOneHistoryRecord(fn)
-    await this.publish(fn)
+    // deploy function
+    if (!dto.params) {
+      await this.addOneHistoryRecord(fn)
+      await this.publish(fn)
+    }
     return fn
   }
 
