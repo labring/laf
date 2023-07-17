36906d0ebf359afcf0c175f7fbbd9be204a9832b
heheer <1239331448@qq.com>
2023-07-17 16:12:16 +0800

    fix(web): fix app state display

--- web/src/pages/home/index.tsx
+++ web/src/pages/home/index.tsx
@@ -1,19 +1,17 @@
 import { useEffect, useState } from "react";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 
 import { APP_PHASE_STATUS } from "@/constants";
 
 import Empty from "./mods/Empty";
 import List from "./mods/List";
 
-import { TApplicationItem } from "@/apis/typing";
 import { ApplicationControllerFindAll } from "@/apis/v1/applications";
 
 export const APP_LIST_QUERY_KEY = ["appListQuery"];
 
 function HomePage() {
   const [shouldRefetch, setShouldRefetch] = useState(true);
-  const queryClient = useQueryClient();
 
   useEffect(() => {
     setTimeout(() => {
@@ -42,39 +40,11 @@ function HomePage() {
     },
   );
 
-  function updatePhase(appid: string, type: keyof typeof APP_PHASE_STATUS) {
-    const newAppListQueryData = appListQuery.data?.data.map((item: TApplicationItem) => {
-      if (item.appid !== appid) return item;
-      return {
-        ...item,
-        phase: type,
-      };
-    });
-    if (appListQuery.data && newAppListQueryData) {
-      queryClient.setQueryData(APP_LIST_QUERY_KEY, {
-        ...appListQuery.data,
-        data: newAppListQueryData,
-      });
-    }
-  }
-
-  if (appListQuery.isLoading) {
-    return null;
-  }
-
   return (appListQuery.data?.data || []).length === 0 ? (
     <Empty />
   ) : (
     <div className="mx-auto mt-10 flex w-11/12 flex-col lg:w-8/12">
-      <List
-        appListQuery={appListQuery}
-        updatePhase={updatePhase}
-        setShouldRefetch={() => {
-          setTimeout(() => {
-            setShouldRefetch(true);
-          }, 1500);
-        }}
-      />
+      <List appListQuery={appListQuery} />
     </div>
   );
 }
