6bf9a77a19d0005eefb7f71eb186b1720e6b9481
HUAHUAI23 <huahua1319873800@outlook.com>
2023-07-31 06:28:46 +0000

    refactor: add post http interceptor

--- server/src/app.interceptor.ts
+++ server/src/app.interceptor.ts
@@ -5,46 +5,70 @@ import {
   CallHandler,
   ForbiddenException,
 } from '@nestjs/common'
-import { Observable, map } from 'rxjs'
+import { Observable, from, mergeMap, of } from 'rxjs'
 import { InterceptorService } from './interceptor/interceptor.service'
 import {
   HttpInterceptorAction,
   HttpInterceptorResponseDto,
 } from './interceptor/dto/http-interceptor.dto'
+import { Response } from 'express'
+import { v4 as uuidv4 } from 'uuid'
 
 @Injectable()
 export class AppInterceptor implements NestInterceptor {
   constructor(private interceptorService: InterceptorService) {}
-  async intercept(
-    context: ExecutionContext,
-    next: CallHandler,
-  ): Promise<Observable<any>> {
-    const response = context.switchToHttp().getResponse()
 
-    const httpInterceptorResponseData: HttpInterceptorResponseDto =
-      await this.interceptorService.httpInterceptor(context)
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const response: Response = context.switchToHttp().getResponse()
+    const requestId = uuidv4()
+    return from(
+      this.interceptorService.processPreInterceptor(context, requestId),
+    ).pipe(
+      mergeMap((preInterceptorData: HttpInterceptorResponseDto) => {
+        if (preInterceptorData.action === HttpInterceptorAction.DENY) {
+          return this.handleDenyAction(response, preInterceptorData)
+        }
 
-    if (httpInterceptorResponseData.action === HttpInterceptorAction.ALLOW) {
-      return next.handle()
+        // post interceptor
+        if (preInterceptorData.action === HttpInterceptorAction.ALLOW) {
+          return next.handle().pipe(
+            mergeMap((data) =>
+              from(
+                this.interceptorService.processPostInterceptor(
+                  context,
+                  requestId,
+                  data,
+                ),
+              ).pipe(
+                mergeMap((postInterceptorData: HttpInterceptorResponseDto) => {
+                  if (
+                    postInterceptorData.action === HttpInterceptorAction.DENY
+                  ) {
+                    return this.handleDenyAction(response, postInterceptorData)
+                  }
+                  return of(data)
+                }),
+              ),
+            ),
+          )
+        }
+      }),
+    )
+  }
+
+  handleDenyAction(
+    response: Response,
+    interceptorData: HttpInterceptorResponseDto,
+  ) {
+    if (interceptorData.rewrite) {
+      response.status(interceptorData.rewrite.status || 200)
+      return of(interceptorData.rewrite.data)
     }
-    if (httpInterceptorResponseData.action === HttpInterceptorAction.DENY) {
-      if (httpInterceptorResponseData.rewrite) {
-        return next.handle().pipe(
-          map(() => {
-            response.status(httpInterceptorResponseData.rewrite.status || 200)
-            return httpInterceptorResponseData.rewrite.data
-          }),
-        )
-      }
-      if (httpInterceptorResponseData.redirect) {
-        return next.handle().pipe(
-          map(() => {
-            response.status(302)
-            response.redirect(httpInterceptorResponseData.redirect)
-          }),
-        )
-      }
-      throw new ForbiddenException("You don't have permission to access")
+    if (interceptorData.redirect) {
+      response.status(302)
+      response.redirect(interceptorData.redirect)
+      return of(null)
     }
+    throw new ForbiddenException("You don't have permission to access")
   }
 }
--- server/src/interceptor/interceptor.service.ts
+++ server/src/interceptor/interceptor.service.ts
@@ -2,16 +2,35 @@ import { HttpService } from '@nestjs/axios'
 import { ExecutionContext, Injectable, Logger } from '@nestjs/common'
 import { HTTP_INTERCEPTOR_TIMEOUT, HTTP_INTERCEPTOR_URL } from 'src/constants'
 import { HttpInterceptorAction } from './dto/http-interceptor.dto'
+import { Response } from 'express'
+import { Request } from 'express'
 
 @Injectable()
 export class InterceptorService {
   constructor(private httpService: HttpService) {}
 
   private readonly logger = new Logger(InterceptorService.name)
+  private readonly HTTP_INTERCEPTOR_URL = HTTP_INTERCEPTOR_URL
+  private readonly HTTP_INTERCEPTOR_TIMEOUT = HTTP_INTERCEPTOR_TIMEOUT
 
-  async httpInterceptor(context: ExecutionContext) {
-    const request = context.switchToHttp().getRequest()
-    const data = {
+  async processPreInterceptor(context: ExecutionContext, requestId: string) {
+    const request: Request = context.switchToHttp().getRequest()
+    const requestData = this.buildRequestData(request, requestId)
+    return this.sendRequestToInterceptor(requestData)
+  }
+
+  async processPostInterceptor(
+    context: ExecutionContext,
+    requestId: string,
+    data: any,
+  ) {
+    const response: Response = context.switchToHttp().getResponse()
+    const responseData = this.buildResponseData(response, requestId, data)
+    return this.sendRequestToInterceptor(responseData)
+  }
+
+  buildRequestData(request: Request, requestId: string) {
+    return {
       url: request.url,
       method: request.method,
       headers: request.headers,
@@ -19,31 +38,34 @@ export class InterceptorService {
       params: request.params,
       query: request.query,
       body: request.body,
+      id: requestId,
+      state: 'pre',
     }
-    if (HTTP_INTERCEPTOR_URL.length === 0) {
-      return {
-        action: HttpInterceptorAction.ALLOW,
-      }
+  }
+  buildResponseData(response: Response, requestId: string, data: any): any {
+    return {
+      id: requestId,
+      headers: response.getHeaders(),
+      data: data,
+      state: 'post',
     }
+  }
 
+  async sendRequestToInterceptor(data: any) {
     try {
-      const httpInterceptorResponse = await this.httpService.axiosRef.post(
-        HTTP_INTERCEPTOR_URL,
+      const response = await this.httpService.axiosRef.post(
+        this.HTTP_INTERCEPTOR_URL,
         data,
-        { timeout: HTTP_INTERCEPTOR_TIMEOUT },
+        { timeout: this.HTTP_INTERCEPTOR_TIMEOUT },
       )
-      console.log(httpInterceptorResponse.data)
-      return httpInterceptorResponse.data
+      return response.data
     } catch (error) {
       if (error.code === 'ECONNABORTED') {
         this.logger.error('Request timed out!')
       } else {
         this.logger.error(error)
       }
-
-      return {
-        action: HttpInterceptorAction.ALLOW,
-      }
+      return { action: HttpInterceptorAction.ALLOW }
     }
   }
 }
