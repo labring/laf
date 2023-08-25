5b392df5d060dd1b87ae834f4de0a9951c976328
HUAHUAI23 <huahua1319873800@outlook.com>
2023-08-25 05:57:48 +0000

    init

--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -3,9 +3,10 @@ import { UserService } from './user.service'
 import { PatService } from './pat.service'
 import { PatController } from './pat.controller'
 import { UserController } from './user.controller'
+import { QuotaServiceTsService } from './quota.service.ts.service'
 
 @Module({
-  providers: [UserService, PatService],
+  providers: [UserService, PatService, QuotaServiceTsService],
   exports: [UserService],
   controllers: [PatController, UserController],
 })
