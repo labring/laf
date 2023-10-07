3878bc092c4e830cbcb7641da26eed89afc885a6
maslow <wangfugen@126.com>
2023-10-07 17:14:17 +0800

    fix(runtime): fix compile error (#1563)

--- runtimes/nodejs/src/support/function-engine/types.ts
+++ runtimes/nodejs/src/support/function-engine/types.ts
@@ -54,7 +54,7 @@ export interface FunctionContext {
   request?: Request
   response?: Response  
   __function_name?: string
-  __is_required: boolean
+  __is_required?: boolean
   [key: string]: any
 }
 
