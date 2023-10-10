0f12c73ce1171f34d91dd5bdac0f5a95f22efddf
0fatal <72899968+0fatal@users.noreply.github.com>
2023-10-10 18:20:37 +0800

    chore(web): add sentry in web(#1570)
    * chore(web): add sentry
    
    * chore(web): enable sentry conditionally

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
+import {
+  createRoutesFromChildren,
+  matchRoutes,
+  useLocation,
+  useNavigationType,
+} from "react-router-dom";
+import * as Sentry from "@sentry/react";
 
 import "focus-visible/dist/focus-visible";
 
 import App from "./App";
+
+if (
+  window.location.hostname.endsWith(".laf.run") ||
+  window.location.hostname.endsWith(".laf.dev")
+) {
+  const commitId = import.meta.env.VITE_GITHUB_SHA;
+
+  Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN,
+    release: `laf@${commitId}`,
+    integrations: [
+      new Sentry.BrowserTracing({
+        routingInstrumentation: Sentry.reactRouterV6Instrumentation(
+          useEffect,
+          useLocation,
+          useNavigationType,
+          createRoutesFromChildren,
+          matchRoutes,
+        ),
+      }),
+      new Sentry.Replay(),
+      // new HttpClient(),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 1.0,
+    // Session Replay
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+  });
+}
 
 // polyfill for aws-sdk
 if (typeof (window as any).global === "undefined") {
