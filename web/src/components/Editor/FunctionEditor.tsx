d099796f8c4e59b796b4bc6fbb495b3a5067aad3
heheer <1239331448@qq.com>
2023-10-11 14:27:06 +0800

    feat: add theme

--- web/src/components/Editor/FunctionEditor.tsx
+++ web/src/components/Editor/FunctionEditor.tsx
@@ -5,87 +5,25 @@ import { buildWorkerDefinition } from "monaco-editor-workers";
 import { COLOR_MODE, Pages } from "@/constants";
 
 import "./userWorker";
+import "./theme";
 
 import { createUrl, createWebSocketAndStartClient } from "./LanguageClient";
 
 import useHotKey, { DEFAULT_SHORTCUTS } from "@/hooks/useHotKey";
 import useGlobalStore from "@/pages/globalStore";
+
 buildWorkerDefinition(
-  "../../../node_modules/monaco-editor-workers/dist/workers/",
+  "../../../../node_modules/monaco-editor-workers/dist/workers/",
   new URL("", window.location.href).href,
   false,
 );
-
-// monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
-//   target: monaco.languages.typescript.ScriptTarget.ESNext,
-//   allowNonTsExtensions: true,
-//   moduleResolution: monaco.languages.typescript.ModuleResolutionKind.NodeJs,
-//   module: monaco.languages.typescript.ModuleKind.CommonJS,
-//   /**
-//    * This option is required to enable the synthetic default imports.
-//    * Support for `import dayjs from 'dayjs'` instead of `import * as dayjs from 'dayjs'`.
-//    * This only affects the editor, not the actual compilation.
-//    */
-//   allowSyntheticDefaultImports: true,
-//   noEmit: true,
-//   allowJs: false,
-//   sourceMap: true,
-//   noImplicitAny: false,
-// });
-
-// monaco?.editor.defineTheme("lafEditorTheme", {
-//   base: "vs",
-//   inherit: true,
-//   rules: [
-//     {
-//       foreground: "#008189",
-//       token: "keyword",
-//     },
-//   ],
-//   colors: {
-//     "editorLineNumber.foreground": "#aaa",
-//     "editorOverviewRuler.border": "#fff",
-//     "editor.lineHighlightBackground": "#F7F8FA",
-//     "scrollbarSlider.background": "#E8EAEC",
-//     "editorIndentGuide.activeBackground": "#fff",
-//     "editorIndentGuide.background": "#eee",
-//   },
-// });
-
-// monaco?.editor.defineTheme("lafEditorThemeDark", {
-//   base: "vs-dark",
-//   inherit: true,
-//   rules: [
-//     {
-//       foreground: "65737e",
-//       token: "punctuation.definition.comment",
-//     },
-//   ],
-//   colors: {
-//     // https://github.com/microsoft/monaco-editor/discussions/3838
-//     "editor.foreground": "#ffffff",
-//     "editor.background": "#202631",
-//     "editorIndentGuide.activeBackground": "#fff",
-//     "editorIndentGuide.background": "#eee",
-//     "editor.selectionBackground": "#101621",
-//     "menu.selectionBackground": "#101621",
-//     "dropdown.background": "#1a202c",
-//     "dropdown.foreground": "#f0f0f0",
-//     "dropdown.border": "#fff",
-//     "quickInputList.focusBackground": "#1a202c",
-//     "editorWidget.background": "#1a202c",
-//     "editorWidget.foreground": "#f0f0f0",
-//     "editorWidget.border": "#1a202c",
-//     "input.background": "#1a202c",
-//     "list.hoverBackground": "#2a303c",
-//   },
-// });
 
 const updateModel = (path: string, value: string, editorRef: any) => {
   const newModel =
     monaco.editor.getModel(monaco.Uri.file(path)) ||
     monaco.editor.createModel(value, "typescript", monaco.Uri.file(path));
 
+  console.log(editorRef.current?.getModel() !== newModel);
   if (editorRef.current?.getModel() !== newModel) {
     editorRef.current?.setModel(newModel);
   }
@@ -153,7 +91,7 @@ function FunctionEditor(props: {
           overviewRulerLanes: 0,
           lineNumbersMinChars: 4,
           fontSize: fontSize,
-          theme: colorMode === COLOR_MODE.dark ? "lafEditorThemeDark" : "lafEditorTheme",
+          theme: "lafEditorTheme",
           scrollBeyondLastLine: false,
         });
         updateModel(path, value, editorRef);
@@ -162,16 +100,18 @@ function FunctionEditor(props: {
         createWebSocketAndStartClient(url);
       };
       start();
-
-      // window.onbeforeunload = () => {
-      //   // On page reload/exit, close web socket connection
-      //   lspWebSocket?.close();
-      // };
-      // return () => {
-      //     // On component unmount, close web socket connection
-      //     lspWebSocket?.close();
-      // };
+    } else if (monacoEl && editorRef.current) {
+      updateModel(path, value, editorRef);
     }
+
+    // window.onbeforeunload = () => {
+    //   // On page reload/exit, close web socket connection
+    //   lspWebSocket?.close();
+    // };
+    // return () => {
+    //     // On component unmount, close web socket connection
+    //     lspWebSocket?.close();
+    // };
 
     return () => {};
   }, [colorMode, path, readOnly, value, fontSize, url]);
@@ -185,6 +125,15 @@ function FunctionEditor(props: {
       });
     }
   }, [onChange]);
+
+  useEffect(() => {
+    if (monacoEl && editorRef.current) {
+      editorRef.current.updateOptions({
+        fontSize: fontSize,
+        theme: "lafEditorTheme",
+      });
+    }
+  }, [colorMode, fontSize]);
 
   return <div style={{ height: height, ...style }} className={className} ref={monacoEl}></div>;
 }
