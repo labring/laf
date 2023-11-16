cfdae63d9d5e29dfb2ffc3cac3427c97118dca34
0fatal <2816813070@qq.com>
2023-11-16 06:19:37 +0000

    chore(server): add field whitelist for validation

--- server/src/function/dto/compile-function.dto.ts
+++ server/src/function/dto/compile-function.dto.ts
@@ -1,10 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger'
-import { IsNotEmpty } from 'class-validator'
+import { IsNotEmpty, IsString } from 'class-validator'
 
 export class CompileFunctionDto {
   @ApiProperty({
     description: 'The source code of the function',
   })
   @IsNotEmpty()
+  @IsString()
   code: string
 }
