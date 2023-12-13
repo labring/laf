c30a8733c6397a4fbf68aa5710ffa4802c46e025
0fatal <2816813070@qq.com>
2023-12-13 07:35:43 +0000

    chore(runtime): ln custom node_module

--- runtimes/nodejs/start.sh
+++ runtimes/nodejs/start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+ln -s $CUSTOM_DEPENDENCY_BASE_PATH/node_modules $PWD/functions/node_modules > /dev/null 2>&1 
+
 # source .env
 echo "****** start service: node $FLAGS --experimental-vm-modules  --experimental-fetch ./dist/index.js *******"
 exec node $FLAGS --experimental-vm-modules  --experimental-fetch ./dist/index.js
