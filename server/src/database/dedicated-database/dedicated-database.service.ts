8adf28f92825c0e9a3417a82547149f1ff100172
0fatal <2816813070@qq.com>
2023-12-21 07:44:52 +0000

    fix miss limit ratio

--- server/src/database/dedicated-database/dedicated-database.service.ts
+++ server/src/database/dedicated-database/dedicated-database.service.ts
@@ -124,9 +124,9 @@ export class DedicatedDatabaseService {
     const { limitCPU, limitMemory, replicas, capacity } = dto
     const name = getDedicatedDatabaseName(appid)
 
-    const requestCPU = limitCPU * region.bundleConf.cpuRequestLimitRatio
+    const requestCPU = limitCPU * region.bundleConf?.cpuRequestLimitRatio || 0.1
     const requestMemory =
-      limitMemory * region.bundleConf.memoryRequestLimitRatio
+      limitMemory * region.bundleConf?.memoryRequestLimitRatio || 0.5
 
     const template = region.deployManifest.database
     const tmpl = _.template(template)
