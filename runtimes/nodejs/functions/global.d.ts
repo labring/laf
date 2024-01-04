9a3aa62611cb2ea5c6e2a8883f5f59ea14208640
0fatal <2816813070@qq.com>
2024-01-04 17:07:03 +0800

    remove extra global type

--- runtimes/nodejs/functions/global.d.ts
+++ runtimes/nodejs/functions/global.d.ts
@@ -1,24 +1,7 @@
-declare class FunctionConsole {
-  private _logs
-  get logs(): any[]
-  log(...params: any[]): void
-}
-
-interface File {
-  fieldname: string
-  originalname: string
-  encoding: string
-  mimetype: string
-  size: number
-  destination: string
-  filename: string
-  path: string
-}
-
 /**
  * The input parameters of cloud function calls
  */
-interface FunctionContext {
+declare interface FunctionContext {
   __function_name: string
 
   /**
@@ -82,14 +65,7 @@ interface FunctionContext {
 }
 
 interface IModule {
-  exports: IExports
-}
-
-interface IExports {
-  /**
-   * The main function, entry of the cloud function
-   */
-  main: (ctx: FunctionContext) => any
+  exports: any
 }
 
 interface IProcess {
@@ -100,12 +76,4 @@ interface IProcess {
 }
 
 declare const module: IModule
-declare const exports: IExports
-declare const console: FunctionConsole
-declare const global: typeof globalThis
 declare const process: IProcess
-
-/**
- *  The main function, entry of the cloud function
- */
-declare function main(ctx: FunctionContext): any
