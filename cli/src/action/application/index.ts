6688d361d233d4d82c45dfed321f79686a01e04c
skyoct <764213885@qq.com>
2024-01-11 15:04:15 +0800

    chore(cli): opt cli code typing prompt (#1804)
    * fix(cli): fix cli env command description
    
    * feat(cli): opt global.d.ts

--- cli/src/action/application/index.ts
+++ cli/src/action/application/index.ts
@@ -14,7 +14,6 @@ import {
   GITIGNORE_FILE,
   GLOBAL_FILE,
   PACKAGE_FILE,
-  RESPONSE_FILE,
   STORAGE_TOKEN_EXPIRE,
   TEMPLATE_DIR,
   TSCONFIG_FILE,
@@ -139,11 +138,6 @@ function initFunction() {
   const fromGlobalFile = path.resolve(templateDir, GLOBAL_FILE)
   const outGlobalFile = path.resolve(typeDir, GLOBAL_FILE)
   fs.writeFileSync(outGlobalFile, fs.readFileSync(fromGlobalFile, 'utf-8'))
-
-  // generate response.d.ts
-  const fromResponseFile = path.resolve(templateDir, RESPONSE_FILE)
-  const outResponseFile = path.resolve(TYPE_DIR, RESPONSE_FILE)
-  fs.writeFileSync(outResponseFile, fs.readFileSync(fromResponseFile, 'utf-8'))
 
   // generate package.json
   const fromPackageFile = path.resolve(templateDir, PACKAGE_FILE)
