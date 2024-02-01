f56fe6451c52b4322b00f5cc79ce8d7b897fb56f
0fatal <2816813070@qq.com>
2024-02-01 07:57:02 +0000

    chore

--- runtimes/nodejs/init.sh
+++ runtimes/nodejs/init.sh
@@ -66,10 +66,18 @@ fi
 echo "Cached dependencies: $CACHED_DEPENDENCIES"
 echo "Dependencies to install: $DEPENDENCIES"
 
+# npm init
+echo '{
+  "scripts": {
+    "postinstall": "sh ./post-install.sh"
+  },
+  "dependencies": {}
+}' > package.json
+
 # if $CACHED_DEPENDENCIES is equal to $DEPENDENCIES
 if [ "$CACHED_DEPENDENCIES" = "$DEPENDENCIES" ]; then
   echo "No dependencies changed since last cache build."
-  [ -n "$DEPENDENCIES" ] && npm install $DEPENDENCIES $NPM_INSTALL_FLAGS
+  npm install $DEPENDENCIES $NPM_INSTALL_FLAGS
   exit 0
 else
   echo "Dependencies changed since last cache build."
@@ -83,36 +91,3 @@ npm install $DEPENDENCIES $NPM_INSTALL_FLAGS
 end_time=$(date +%s)
 elapsed_time=$(expr $end_time - $start_time)
 echo "Installed dependencies in $elapsed_time seconds."
-
-### cache node_modules ###
-# if $NODE_MODULES_PUSH_URL is not empty
-if [ -n "$NODE_MODULES_PUSH_URL" ]; then
-  # temporarily disable set -e
-  set +e
-
-  start_time=$(date +%s)
-  echo $DEPENDENCIES > node_modules/.dependencies
-  echo "Uploading node_modules to $NODE_MODULES_PUSH_URL"
-
-  # tar `node_modules` to node_modules.tar
-  tar -cf node_modules.tar ./node_modules
-
-  end_time_1=$(date +%s)
-  elapsed_time=$(expr $end_time_1 - $start_time)
-  echo "Compressed node_modules in $elapsed_time seconds."
-
-  # upload node_modules.tar to $NODE_MODULES_PUSH_URL
-  curl -sSfL -X PUT -T node_modules.tar $NODE_MODULES_PUSH_URL
-
-
-  if [ $? -ne 0 ]; then
-    echo "Failed to upload node_modules cache."
-  else
-    end_time_2=$(date +%s)
-    elapsed_time_2=$(expr $end_time_2 - $end_time)
-    echo "Uploaded node_modules.tar in $elapsed_time_2 seconds."
-  fi
-
-  # re-enable set -e
-  set -e
-fi
