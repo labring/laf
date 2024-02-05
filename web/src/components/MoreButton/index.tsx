bcde132fd872be9ef033d059055b92d5e20c5fff
heheer <71265218+newfish-cmyk@users.noreply.github.com>
2024-02-05 14:21:21 +0800

    feat(web): add function list context menu (#1825)
    * feat(web): add function list context menu
    
    * fix popover trigger click
    
    * fix delete with new function render & fix contextmenu hide after delete

--- web/src/components/MoreButton/index.tsx
+++ web/src/components/MoreButton/index.tsx
@@ -1,5 +1,6 @@
 import {
   Box,
+  Button,
   Popover,
   PopoverContent,
   PopoverTrigger,
@@ -17,8 +18,9 @@ export default function MoreButton(props: {
   label: string;
   maxWidth?: string;
   className?: string;
+  refItem?: React.RefObject<HTMLDivElement>;
 }) {
-  const { children, isHidden, maxWidth, label = t("openPopover"), className } = props;
+  const { children, isHidden, maxWidth, label = t("openPopover"), className, refItem } = props;
   const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <div className={clsx("flex group-hover:visible ", isHidden ? "invisible" : "visible")}>
@@ -26,19 +28,16 @@ export default function MoreButton(props: {
         isOpen={isOpen}
         onOpen={onOpen}
         onClose={onClose}
-        closeOnBlur={true}
         placement="bottom"
       >
         <Tooltip aria-label="tooltip" placement="bottom" label={label}>
           <Box display="inline-block">
             <PopoverTrigger>
-              <div className="px-1">
-                <MoreIcon className="cursor-pointer align-middle" fontSize={12} />
-              </div>
+              <Button variant="none" p={0} minW={0} h={0} w={5}><MoreIcon className="cursor-pointer align-middle" fontSize={12} /></Button>
             </PopoverTrigger>
           </Box>
         </Tooltip>
-        <PopoverContent p="2" maxWidth={maxWidth ? maxWidth : "100px"} className={className}>
+        <PopoverContent p="2" maxWidth={maxWidth ? maxWidth : "120px"} className={className}>
           <div className="flex justify-around">{children}</div>
         </PopoverContent>
       </Popover>
