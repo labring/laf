2364f57b432fbf93727e3f5872af4e28d8a22da2
0fatal <72899968+0fatal@users.noreply.github.com>
2024-02-22 14:45:12 +0800

    feat(server): support auth methods of email (#1867)
    * feat(server): support email auth

--- server/src/authentication/email/email.controller.ts
+++ server/src/authentication/email/email.controller.ts
@@ -5,13 +5,22 @@ import { ResponseUtil } from 'src/utils/response'
 import { SendEmailCodeDto } from '../dto/send-email-code.dto'
 import { EmailService } from './email.service'
 import { GetClientIPFromRequest } from 'src/utils/getter'
+import { UserService } from 'src/user/user.service'
+import { EmailSigninDto } from '../dto/email-signin.dto'
+import { EmailVerifyCodeType } from '../entities/email-verify-code'
+import { AuthenticationService } from '../authentication.service'
+import { AuthBindingType, AuthProviderBinding } from '../entities/types'
 
 @ApiTags('Authentication')
 @Controller('auth')
 export class EmailController {
   private readonly logger = new Logger(EmailController.name)
 
-  constructor(private readonly emailService: EmailService) {}
+  constructor(
+    private readonly emailService: EmailService,
+    private readonly authService: AuthenticationService,
+    private readonly userService: UserService,
+  ) {}
 
   /**
    * send email code
@@ -29,5 +38,51 @@ export class EmailController {
       return ResponseUtil.error(err)
     }
     return ResponseUtil.ok('success')
+  }
+
+  /**
+   * Signin by email and verify code
+   */
+  @ApiOperation({ summary: 'Signin by email and verify code' })
+  @ApiResponse({ type: ResponseUtil })
+  @Post('email/signin')
+  async signin(@Body() dto: EmailSigninDto) {
+    const { email, code } = dto
+    // check if code valid
+    const err = await this.emailService.validateCode(
+      email,
+      code,
+      EmailVerifyCodeType.Signin,
+    )
+    if (err) return ResponseUtil.error(err)
+
+    // check if user exists
+    const user = await this.userService.findOneByEmail(email)
+    if (user) {
+      const token = this.emailService.signin(user)
+      return ResponseUtil.ok(token)
+    }
+
+    // user not exist
+    const provider = await this.authService.getEmailProvider()
+    if (provider.register === false) {
+      return ResponseUtil.error('user not exists')
+    }
+
+    // check if username and password is needed
+    let signupWithUsername = false
+    const bind = provider.bind as any as AuthProviderBinding
+    if (bind.username === AuthBindingType.Required) {
+      const { username, password } = dto
+      signupWithUsername = true
+      if (!username || !password) {
+        return ResponseUtil.error('username and password is required')
+      }
+    }
+
+    // user not exist, signup and signin
+    const newUser = await this.emailService.signup(dto, signupWithUsername)
+    const data = this.emailService.signin(newUser)
+    return ResponseUtil.ok(data)
   }
 }
--- server/src/function/entities/cloud-function.ts
+++ server/src/function/entities/cloud-function.ts
@@ -1,11 +1,23 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger'
 import { ObjectId } from 'mongodb'
 
-export type CloudFunctionSource = {
+export class CloudFunctionSource {
+  @ApiProperty()
   code: string
+
+  @ApiProperty()
   compiled: string
+
+  @ApiPropertyOptional()
   uri?: string
+
+  @ApiProperty()
   version: number
+
+  @ApiPropertyOptional()
   hash?: string
+
+  @ApiPropertyOptional()
   lang?: string
 }
 
@@ -19,15 +31,36 @@ export enum HttpMethod {
 }
 
 export class CloudFunction {
+  @ApiProperty({ type: String })
   _id?: ObjectId
+
+  @ApiProperty()
   appid: string
+
+  @ApiProperty()
   name: string
+
+  @ApiProperty({ type: CloudFunctionSource })
   source: CloudFunctionSource
+
+  @ApiProperty()
   desc: string
+
+  @ApiProperty({ type: String, isArray: true })
   tags: string[]
+
+  @ApiProperty({ enum: HttpMethod, isArray: true })
   methods: HttpMethod[]
+
+  @ApiPropertyOptional()
   params?: any
+
+  @ApiProperty()
   createdAt: Date
+
+  @ApiProperty()
   updatedAt: Date
+
+  @ApiProperty({ type: String })
   createdBy: ObjectId
 }
