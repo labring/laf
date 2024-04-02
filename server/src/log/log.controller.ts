babef61825cf690b8f5159070c5380e1fa453605
nightwhite <nightwhite634@gmail.com>
2024-04-03 01:10:34 +0800

    Update log.controller.ts

--- server/src/log/log.controller.ts
+++ server/src/log/log.controller.ts
@@ -138,12 +138,14 @@ export class LogController {
       const streamsEnded = new Set<string>()
 
       const timerId = setInterval(() => {
+        console.log("data send...")
         subscriber.next('data: \n' as unknown as MessageEvent)
       }, 60000)
 
       const destroyStream = () => {
         combinedLogStream?.removeAllListeners()
         combinedLogStream?.destroy()
+        console.log("clearInterval timerId")
         clearInterval(timerId)
       }
 
