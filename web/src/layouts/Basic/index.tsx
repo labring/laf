9c6721bddbd57a472de59fe62550f6877d3bda5f
HUAHUAI23 <lim@outlook.com>
2024-05-14 08:40:53 +0000

    feat(web): add notification for sealaf

--- web/src/layouts/Basic/index.tsx
+++ web/src/layouts/Basic/index.tsx
@@ -1,7 +1,9 @@
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import { AiFillHeart } from "react-icons/ai";
 import { Outlet } from "react-router-dom";
 import { Center, Spinner } from "@chakra-ui/react";
+
+import i18n from "@/utils/i18n";
 
 import Warn from "./RealNameWarn";
 
@@ -10,9 +12,13 @@ import useAuthStore from "@/pages/auth/store";
 import useGlobalStore from "@/pages/globalStore";
 import useSiteSettingStore from "@/pages/siteSetting";
 
+const TOAST_KEY = "sealafLastToastTime";
+const ONE_DAY_IN_MS = 24 * 60 * 60 * 1000; // 24 hours in milliseconds
+
 export default function BasicLayout() {
   const { init, loading, userInfo } = useGlobalStore((state) => state);
   const { siteSettings } = useSiteSettingStore((state) => state);
+  const { showInfo } = useGlobalStore(({ showInfo }) => ({ showInfo }));
   const { phoneProvider } = useAuthStore((state) => ({
     phoneProvider: state.phoneProvider,
   }));
@@ -20,6 +26,36 @@ export default function BasicLayout() {
   useEffect(() => {
     init();
   }, [init]);
+
+  const showNotification = useCallback(() => {
+    const lastToastTime = localStorage.getItem(TOAST_KEY);
+    const now = new Date().getTime();
+
+    if (!lastToastTime || now - parseInt(lastToastTime) > ONE_DAY_IN_MS) {
+      const message =
+        i18n.language === "zh"
+          ? siteSettings?.sealaf_notification?.metadata?.message?.zh
+          : siteSettings?.sealaf_notification?.metadata?.message?.en;
+
+      showInfo(
+        <a
+          href={siteSettings?.sealaf_notification?.metadata?.gotoSite}
+          className="font-bold underline transition ease-in-out"
+        >
+          {message}
+        </a>,
+        9000,
+        true,
+      );
+      localStorage.setItem(TOAST_KEY, now.toString());
+    }
+  }, [showInfo, siteSettings]);
+
+  useEffect(() => {
+    if (!loading && siteSettings?.sealaf_notification?.value === "on") {
+      showNotification();
+    }
+  }, [loading, siteSettings, showNotification]);
 
   return (
     <div>
