e773231768cba97855fa00bdf849203bc505d33c
HUAHUAI23 <huahua1319873800@outlook.com>
2024-07-16 12:12:19 +0000

    chore(runtime): Fix strange dependency installation during runtime startup

--- runtimes/nodejs/start.sh
+++ runtimes/nodejs/start.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-ln -s $CUSTOM_DEPENDENCY_BASE_PATH/node_modules $PWD/functions/node_modules > /dev/null 2>&1 
+ln -s $CUSTOM_DEPENDENCY_BASE_PATH/node_modules $PWD/functions/node_modules >/dev/null 2>&1
 
 # generate package.json
-(
-  cd $CUSTOM_DEPENDENCY_BASE_PATH
-  npm install $DEPENDENCIES $NPM_INSTALL_FLAGS > /dev/null 2>&1
-)
+# (
+#   cd $CUSTOM_DEPENDENCY_BASE_PATH
+#   npm install $DEPENDENCIES $NPM_INSTALL_FLAGS > /dev/null 2>&1
+# )
 
 # source .env
 echo "****** start service: node $FLAGS --experimental-vm-modules  --experimental-fetch ./dist/index.js *******"
-exec node $FLAGS --experimental-vm-modules  --experimental-fetch ./dist/index.js
+exec node $FLAGS --experimental-vm-modules --experimental-fetch ./dist/index.js
